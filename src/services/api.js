9e625cae71df120cfc6c2f3dc26ad1603610d415
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-01-18 14:58:25 -0300

    fix base url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://192.168.0.16:3000',
+    baseURL: `http://192.168.15.6:3000/v1.0.0`,
 });
 
 export default api;
