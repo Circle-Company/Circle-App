1bebfe699f417e31cea32aeba2f978cd9c4ef096
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-01-18 15:09:09 -0300

    refactor

--- src/components/headers/home/Right.tsx
+++ src/components/headers/home/Right.tsx
@@ -3,35 +3,18 @@ import { Pressable, View, Text, Animated} from 'react-native';
 import ColorTheme from '../../../layout/constants/colors';
 import {useNavigation} from '@react-navigation/native';
 import Camera from '../../../assets/icons/svgs/camera.svg'
+import InboxIcon from '../../../assets/icons/svgs/tray.svg'
 import sizes from '../../../layout/constants/sizes';
 import fonts from '../../../layout/constants/fonts';
+import ButtonStandart from '../../buttons/button-standart';
 
 export default function HeaderRightHome() {
-
-    var animatedScale = React.useRef(new Animated.Value(1)).current
-    React.useEffect(() => { animatedScale.setValue(1) }, [])
-    const HandleButtonAnimation = () => {
-        animatedScale.setValue(0.8)
-        Animated.spring(animatedScale, {
-            toValue: 1,
-            bounciness: 12,
-            speed: 10,
-            useNativeDriver: true
-        }).start()
-    }
-
     const navigation = useNavigation()
 
     const container: any = {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: ColorTheme().backgroundDisabled,
-        marginRight: sizes.margins['3sm'],
-        width: sizes.buttons.width*0.45,
-        height: sizes.buttons.height*0.5,
-        borderRadius: (sizes.buttons.height*0.5)/2
+        flexDirection: 'row'
     }
+
     const text: any = {
         fontSize: fonts.size.footnote,
         fontFamily: fonts.family.Bold,
@@ -40,21 +23,41 @@ export default function HeaderRightHome() {
     const textContainer = {
         marginRight: sizes.margins['2sm']
     }
+    const badge_style = {
+        zIndex: 10,
+        position: 'absolute',
+        top: 0,
+        right: -4,
+        width: 17,
+        height: 17,
+        borderRadius: 10,
+        backgroundColor: ColorTheme().error,
+        borderWidth: 3,
+        borderColor: ColorTheme().background
+    }
 
-    async function onPress() {
-        HandleButtonAnimation()
+    async function onPressNewMoment() {
         console.log('New Moment Pressed')
-        navigation.navigate('SettingsNavigator')
+    }
+
+    async function onPressInbox() {
+        navigation.navigate('InboxNavigator', {screen: 'Inbox'})
+        console.log('New Moment Pressed')
     }
     return(
-        <Animated.View style={{transform: [{ scale: animatedScale }] }}>
-            <Pressable style={container} onPress={() => {onPress()}}>
-                <View style={textContainer}>
-                    <Text style={text}>New Moment</Text>
-                </View>
-                
-                <Camera fill={String(ColorTheme().text)} width={16} height={16}/>
-            </Pressable>
-        </Animated.View>  
+        <View style={container}>
+        <ButtonStandart action={onPressNewMoment} width={sizes.buttons.width*0.45} backgroundColor={String(ColorTheme().backgroundDisabled)}>
+            <View style={textContainer}>
+                <Text style={text}>New Moment</Text>
+            </View>
+            <Camera fill={String(ColorTheme().text)} width={16} height={16}/>
+        </ButtonStandart>  
+        <ButtonStandart action={onPressInbox} backgroundColor={String(ColorTheme().backgroundDisabled)}>
+            <View style={badge_style}/>
+            <InboxIcon fill={String(ColorTheme().text)} width={18} height={18}/>
+        </ButtonStandart>                  
+        </View>
+
+
     )
 }
