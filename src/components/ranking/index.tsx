aa7be36fa5923638c6b9e146e042a32211544392
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-01-18 15:03:58 -0300

    add famous users ranking

---
+++ src/components/ranking/index.tsx
@@ -0,0 +1,9 @@
+import header from "./components/header/ranking-header";
+import header_left from "./components/header/ranking-header_left";
+import header_right from "./components/header/ranking-header_right";
+
+export const Ranking = {
+    Header: header,
+    HeaderLeft: header_left,
+    HeaderRight: header_right
+}
---
+++ src/components/ranking/ranking-types.tsx
@@ -0,0 +1,30 @@
+export type RankingMainRootProps = {
+    data: any,
+    children: React.ReactNode
+}
+export type RankingCenterRootProps = {
+    children?: React.ReactNode
+}
+export type RankingTopRootProps = {
+    children: React.ReactNode
+}
+export type RankingTopLeftRootProps = {
+    children: React.ReactNode
+}
+export type RankingTopRightRootProps = {
+    children: React.ReactNode
+}
+
+export type RankingHeaderProps = {
+    children: React.ReactNode
+}
+export type RankingHeaderRightProps = {
+    children: React.ReactNode
+}
+export type RankingHeaderLeftProps = {
+    text?: string,
+    number?: number,
+    children?: React.ReactNode
+}
+export type RankingDescriptionProps = {
+}
