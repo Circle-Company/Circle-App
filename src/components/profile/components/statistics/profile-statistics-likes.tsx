cc597292f2ab2e92d79c0a4965f20c1f13acb991
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-01-18 15:01:51 -0300

    add styles

--- src/components/profile/components/statistics/profile-statistics-likes.tsx
+++ src/components/profile/components/statistics/profile-statistics-likes.tsx
@@ -3,14 +3,41 @@ import { ProfileStatisticsLikesProps } from "../../profile-types";
 import { useProfileContext } from "../../profile-context";
 import { Text } from "../../../Themed";
 import NumberConversor from "../../../../algorithms/numberConversor";
-import {statistics_container, statistics_number, statistics_text} from './profile-statistics-styles'
+import sizes from "../../../../layout/constants/sizes";
+import fonts from "../../../../layout/constants/fonts";
+import { useColorScheme } from "react-native";
+import ColorTheme, {colors} from "../../../../layout/constants/colors";
 
 export default function statistics_likes ({}: ProfileStatisticsLikesProps) {
     const {user} = useProfileContext()
+    const isDarkMode = useColorScheme() === 'dark'
+
+    const container: any = {
+        width: sizes.screens.width/4,
+        backgroundColor: isDarkMode? colors.gray.grey_08: colors.gray.grey_02,
+        paddingHorizontal: sizes.paddings["1sm"],
+        marginTop: sizes.margins["1sm"],
+        paddingVertical: sizes.paddings['1sm']*0.8,
+        borderRadius: sizes.borderRadius["1md"],
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+
+    const num_style: any = {
+        fontFamily: fonts.family.Bold,
+        fontSize: fonts.size.headline * 0.9,
+        color: ColorTheme().text
+    }
+    const text_style: any = {
+        top: 1,
+        fontFamily: fonts.family.Semibold,
+        fontSize: fonts.size.body*0.8,
+        color: ColorTheme().textDisabled
+    }
     return (
-        <View style={statistics_container}>
-            <Text style={statistics_number}>{ NumberConversor(user.statistics.likes)}</Text>
-            <Text style={statistics_text}>Likes</Text>
+        <View style={container}>
+            <Text style={num_style}>{ NumberConversor(user.statistics.total_likes_num)}</Text>
+            <Text style={text_style}>Likes</Text>
         </View>
     )
 } 
--- src/components/profile/components/statistics/profile-statistics-styles.tsx
+++ src/components/profile/components/statistics/profile-statistics-styles.tsx
@@ -1,13 +1,37 @@
+import React from "react"
 import fonts from "../../../../layout/constants/fonts"
+import { Text, View } from "../../../Themed"
 
-export const statistics_container: any = {
-    alignItems: 'center',
+
+type StatisticsProps = {
+    children: React.ReactNode
 }
-export const statistics_number: any = {
-    fontSize: fonts.size.title2,
-    fontFamily: fonts.family.Bold
+
+function StatisticsContainer({children}: StatisticsProps): React.ReactNode{
+    const container:any = {
+        alignItems: 'center',
+    }
+    return (<View style={container}> {children} </View>)
 }
-export const statistics_text: any = {
-    fontSize: fonts.size.body,
-    fontFamily: fonts.family.Medium,
+
+function StatisticsNumber({children}: StatisticsProps): React.ReactNode {
+    const statistics_number: any = {
+        fontSize: fonts.size.title2,
+        fontFamily: fonts.family.Bold
+    }
+    return(<Text style={statistics_number}></Text>)
+}
+
+function StatisticsText({children}: StatisticsProps): React.ReactNode {
+    const statistics_text: any = {
+        fontSize: fonts.size.body,
+        fontFamily: fonts.family.Medium,
+    }
+    return(<Text style={statistics_text}></Text>)
+}
+
+export const StatisticsStyles = {
+    Container: StatisticsContainer,
+    Text: StatisticsText,
+    Number: StatisticsNumber
 }
