38e7e07e5bf1491c341a7a7e248d6e5353b81841
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-05-22 11:31:56 -0300

    add: use persisted to store

--- src/contexts/Preferences/language.tsx
+++ src/contexts/Preferences/language.tsx
@@ -1,23 +1,34 @@
 import React from "react"
 import { LanguagesCodesType } from "../../locales/LanguageTypes"
 import i18n from '../../locales/i18n'
+import PersistedContext from "../Persisted"
 
 type LanguageProviderProps = { children: React.ReactNode }
 type languageContextData = {
+    changePrimaryLanguage: (LanguageCode: LanguagesCodesType) => void
     changeAppLanguage: (languageCode: LanguagesCodesType) => void
     changeContentLanguage: (LanguageCode: LanguagesCodesType) => void
 }
 const LanguageContext = React.createContext<languageContextData>({} as languageContextData)
 
 export function Provider({ children }: LanguageProviderProps) {
+    const { session } = React.useContext(PersistedContext)
 
     function changeAppLanguage(LanguageCode: LanguagesCodesType) {
         i18n.changeLanguage(LanguageCode)
+        session.preferences?.language.setAppLanguage(LanguageCode)
+    }
+    function changePrimaryLanguage(LanguageCode: LanguagesCodesType) {
+        session.preferences?.language.setPrimaryLanguage(LanguageCode)
     }
     function changeContentLanguage(LanguageCode: LanguagesCodesType) {
-        i18n.changeLanguage(LanguageCode)
+        session.preferences?.content.setTranslationLanguage(LanguageCode)
     }
 
-    const contextValue = { changeAppLanguage, changeContentLanguage }
+    const contextValue = {
+        changeAppLanguage,
+        changeContentLanguage,
+        changePrimaryLanguage
+    }
     return <LanguageContext.Provider value={contextValue}>{children}</LanguageContext.Provider>
 }
