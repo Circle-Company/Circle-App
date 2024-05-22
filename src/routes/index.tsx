869ccc84453ff01f3783829bd3ffda7b2c119edf
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-05-22 11:33:48 -0300

    refactor: checkAuthenticated function

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,25 +5,18 @@ import AuthContext from "../contexts/auth"
 import LoadingScreen from "../pages/auth/Loading";
 
 export default function Routes() {
-    const { signed, checkAuthenticated } = React.useContext(AuthContext);
-    const [authChecked, setAuthChecked] = React.useState(false);
+    const {signed, checkAuthenticated } = React.useContext(AuthContext);
     const [redirectTo, setRedirectTo] = React.useState<string | null>(null);
   
     React.useEffect(() => {
       const checkAuth = async () => {
-        const isAuthenticated = await checkAuthenticated();
-        setAuthChecked(true);
-  
-        if (isAuthenticated) {
-          setRedirectTo('AppRoute');
-        } else {
-          setRedirectTo('AuthRoute');
-        }
-      };
-  
-      checkAuth();
-    }, [checkAuthenticated]);
-  
-    if (!authChecked || !redirectTo) return <LoadingScreen/>
-    return redirectTo === 'AppRoute' ? <AppRoute /> : <AuthRoute />;
+        const isAuthenticated = await checkAuthenticated()
+        if (isAuthenticated) setRedirectTo('App')
+        else setRedirectTo('Auth')
+      }; checkAuth()
+    }, [checkAuthenticated])
+
+
+    if (!signed || !redirectTo) return <LoadingScreen/>
+    return redirectTo === 'App' ? <AppRoute /> : <AuthRoute />
   }
