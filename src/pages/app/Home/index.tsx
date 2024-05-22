c28fa4d6a9e5a2101385e0fe73e597aa496677e6
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-05-22 10:40:37 -0300

    add: keyboard animation

--- src/pages/app/Home/index.tsx
+++ src/pages/app/Home/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react'
-import { StatusBar, useColorScheme, Keyboard } from 'react-native'
+import { StatusBar, useColorScheme, Keyboard, Animated} from 'react-native'
 import { View } from '../../../components/Themed'
 import ColorTheme, { colors } from '../../../layout/constants/colors'
 import ListMoments from '../../../features/list-moments'
@@ -7,11 +7,13 @@ import AuthContext from '../../../contexts/auth'
 import FeedContext from '../../../contexts/Feed'
 import { Comments } from '../../../components/comment'
 import sizes from '../../../layout/constants/sizes'
+import { useKeyboardAnimation } from 'react-native-keyboard-controller'
 
 export default function HomeScreen() {
   const isDarkMode = useColorScheme() === 'dark';
-  const { getDeviceInfo, deviceInfo, user} = React.useContext(AuthContext);
+  const { getDeviceInfo, deviceInfo } = React.useContext(AuthContext);
   const { commentEnabled, setCommentEnabled, showKeyboard} = React.useContext(FeedContext);
+  const { height } = useKeyboardAnimation()
 
   const bottomContainerRef = useRef(null);
   useEffect(() => {
@@ -29,11 +31,14 @@ export default function HomeScreen() {
   };
 
   const bottomContainer = {
+    bottom: 0,
     paddingVertical: sizes.paddings['1sm'] * 0.4,
     paddingHorizontal: sizes.paddings['2sm'] * 0.8,
     width: sizes.screens.width,
     borderTopWidth: sizes.borders['1md'] * 0.7,
     borderColor: isDarkMode ? colors.transparent.white_10 : colors.transparent.black_10,
+    backgroundColor: ColorTheme().background,
+    transform: [{ translateY: height }],
   };
 
   return (
@@ -41,14 +46,14 @@ export default function HomeScreen() {
       <StatusBar backgroundColor={String(ColorTheme().background)} barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <ListMoments />
       {commentEnabled && (
-        <View ref={bottomContainerRef} style={bottomContainer}>
+        <Animated.View ref={bottomContainerRef} style={bottomContainer}>
               <Comments.Input
                 preview={false}
                 color={isDarkMode ? colors.gray.white.toString() : colors.gray.black.toString()}
                 backgroundColor={String(isDarkMode ? colors.gray.grey_09 : colors.gray.grey_01)}
                 autoFocus={showKeyboard? true: false}
               />
-        </View>
+        </Animated.View>
       )}
     </View>
   );
