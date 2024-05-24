89500a5a118a578aea5f240fd4791ee668d502ca
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-05-24 14:08:56 -0300

    add: zustand to store control

--- src/contexts/Persisted/persistedAccount.ts
+++ src/contexts/Persisted/persistedAccount.ts
@@ -1,67 +1,72 @@
-import { AccountDataType } from "./types"
-import { storage } from "../../store"
-import { storageKeys } from "./storageKeys"
-
+import create from 'zustand';
+import { AccountDataType } from './types';
+import { storage } from '../../store'
+import { storageKeys } from './storageKeys'
 const storageKey = storageKeys().account
 
-export class persistedAccountClass {
-    public blocked: boolean
-    public muted: boolean
-    public last_active_at: string
-    public last_login_at: string
-
-    constructor({
-        blocked,
-        muted,
-        last_active_at,
-        last_login_at
-    }: AccountDataType) {
-        this.blocked = blocked
-        this.muted = muted
-        this.last_active_at = last_active_at
-        this.last_login_at = last_login_at
-    }
-
-    public setBlocked(value: boolean) {
-        this.blocked = value
-        storage.set(storageKey.blocked, value)
-    }
-    public setMuted(value: boolean) {
-        this.muted = value
-        storage.set(storageKey.muted, value)
-    }
-    public setLastActiveAt(value: string) {
-        this.last_active_at = value
-        storage.set(storageKey.last_active_at, value)
-    }
-    public setLastLoginAt(value: string) {
-        this.last_login_at = value
-        storage.set(storageKey.last_login_at, value)
-    }
-
-    public storeAccount(value: AccountDataType) {
-        this.setBlocked(value.blocked)
-        this.setMuted(value.muted)
-        this.setLastActiveAt(value.last_active_at)
-        this.setLastLoginAt(value.last_login_at)
-    }
-
-    public loadAccountFromStorage() {
-        const blocked = storage.getBoolean(storageKey.blocked)
-        const muted = storage.getBoolean(storageKey.muted)
-        const last_active_at = storage.getString(storageKey.last_active_at)
-        const last_login_at = storage.getString(storageKey.last_login_at)
-
-        if(blocked) this.blocked = blocked
-        if(muted) this.muted = muted
-        if(last_active_at) this.last_active_at = last_active_at
-        if(last_login_at) this.last_login_at = last_login_at
-    }
-
-    public removeAccountFromStorage() {
-        storage.delete(storageKey.blocked)
-        storage.delete(storageKey.muted)
-        storage.delete(storageKey.last_active_at)
-        storage.delete(storageKey.last_login_at)
-    }
+export interface AccountState extends AccountDataType {
+    setBlocked: (value: boolean) => void;
+    setMuted: (value: boolean) => void;
+    setLastActiveAt: (value: string) => void;
+    setLastLoginAt: (value: string) => void;
+    setAccount: (value: AccountDataType) => void
+    loadAccountFromStorage: () => void;
+    removeAccountFromStorage: () => void;
 }
+
+export const useAccountStore = create<AccountState>((set) => ({
+    blocked: storage.getBoolean(storageKey.blocked) || false,
+    muted: storage.getBoolean(storageKey.muted) || false,
+    last_active_at: storage.getString(storageKey.last_active_at) || new Date().toString(),
+    last_login_at: storage.getString(storageKey.last_login_at) || new Date().toString(),
+
+    setBlocked: (value: boolean) => {
+        storage.set(storageKey.blocked, value);
+        set({ blocked: value });
+    },
+    setMuted: (value: boolean) => {
+        storage.set(storageKey.muted, value);
+        set({ muted: value });
+    },
+    setLastActiveAt: (value: string) => {
+        storage.set(storageKey.last_active_at, value);
+        set({ last_active_at: value });
+    },
+    setLastLoginAt: (value: string) => {
+        storage.set(storageKey.last_login_at, value);
+        set({ last_login_at: value });
+    },
+    setAccount: (value: AccountDataType) => {
+        set({
+            blocked: value.blocked,
+            muted: value.muted,
+            last_active_at: value.last_active_at,
+            last_login_at: value.last_login_at
+        })
+        storage.set(storageKey.blocked, value.blocked)
+        storage.set(storageKey.muted, value.blocked)
+        storage.set(storageKey.last_active_at, value.last_active_at)
+        storage.set(storageKey.last_login_at, value.last_login_at)
+    },
+    loadAccountFromStorage: () => {
+        set({
+            blocked: storage.getBoolean(storageKey.blocked) || false,
+            muted: storage.getBoolean(storageKey.muted) || false,
+            last_active_at: storage.getString(storageKey.last_active_at) || new Date().toString(),
+            last_login_at: storage.getString(storageKey.last_login_at) || new Date().toString(),
+        });
+    },
+    removeAccountFromStorage: () => {
+        storage.delete(storageKey.blocked);
+        storage.delete(storageKey.muted);
+        storage.delete(storageKey.last_active_at);
+        storage.delete(storageKey.last_login_at);
+
+        set({
+            blocked: false,
+            muted: false,
+            last_active_at: new Date().toString(),
+            last_login_at: new Date().toString(),
+        });
+    },
+}));
--- src/contexts/Persisted/persistedPreferences.ts
+++ src/contexts/Persisted/persistedPreferences.ts
@@ -1,80 +1,133 @@
-import { PreferencesDataStorageType, ContentDataStorageType, LanguageDataStorageType} from "./types"
-import { storage } from "../../store"
-import { storageKeys } from "./storageKeys"
+import create from 'zustand';
+import { PreferencesDataStorageType } from './types'
+import { storage } from '../../store';
+import { storageKeys } from './storageKeys';
 
 const storageKey = storageKeys().preferences
 
-export class persistedPreferencesClass {
-    public language : LanguageDataStorageType
-    public content : ContentDataStorageType
-
-    constructor({ language, content }: PreferencesDataStorageType) {
-        this.language = language
-        this.content = content
-    }
-
-    public setPrimaryLanguage(value: string) {
-        this.language.primaryLanguage = value
-        storage.set(storageKey.primaryLanguage, value)
-    }
-    public setAppLanguage(value: string) {
-        this.language.appLanguage = value
-        storage.set(storageKey.appLanguage, value)
-    }
-    public setDisableAutoPlay(value: boolean) {
-        this.content.disableAutoplay = value
-        storage.set(storageKey.autoplay, value)
-    }
-    public setDisableHaptics(value: boolean) {
-        this.content.disableHaptics = value
-        storage.set(storageKey.haptics, value)
-    }
-    public setDisableTranslation(value: boolean) {
-        this.content.disableTranslation = value
-        storage.set(storageKey.translation, value)
-    }
-    public setTranslationLanguage(value: string) {
-        this.content.translationLanguage = value
-        storage.set(storageKey.translationLanguage, value)
-    }
-    public setLanguage(value: LanguageDataStorageType) {
-        this.setAppLanguage(value.appLanguage)
-        this.setPrimaryLanguage(value.primaryLanguage)
-    }
-
-    public setContent(value: ContentDataStorageType) {
-        this.setDisableAutoPlay(value.disableAutoplay)
-        this.setDisableHaptics(value.disableHaptics)
-        this.setDisableTranslation(value.disableTranslation)
-        this.setTranslationLanguage(value.translationLanguage)
-    }
-
-    public storePreferences(value: PreferencesDataStorageType) {
-        this.setLanguage(value.language)
-        this.setContent(value.content)
-    }
-
-    public loadPreferencesFromStorage() {
-        const appLanguage = storage.getString(storageKey.appLanguage)
-        const primaryLanguage = storage.getString(storageKey.primaryLanguage)
-        const disableAutoplay = storage.getBoolean(storageKey.autoplay)
-        const disableHaptics = storage.getBoolean(storageKey.haptics)
-        const disableTranslation = storage.getBoolean(storageKey.translation)
-        const translationLanguage = storage.getString(storageKey.translationLanguage)
-        if(appLanguage) this.setAppLanguage(appLanguage)
-        if(primaryLanguage) this.setPrimaryLanguage(primaryLanguage)
-        if(disableAutoplay) this.setDisableAutoPlay(disableAutoplay)
-        if(disableHaptics) this.setDisableHaptics(disableHaptics)
-        if(disableTranslation) this.setDisableTranslation(disableTranslation)
-        if(translationLanguage) this.setTranslationLanguage(translationLanguage)
-    }
-
-    public removePreferencesFromStorage() {
-        storage.delete(storageKey.appLanguage)
-        storage.delete(storageKey.primaryLanguage)
-        storage.delete(storageKey.autoplay)
-        storage.delete(storageKey.haptics)
-        storage.delete(storageKey.translation)
-        storage.delete(storageKey.translationLanguage)
-    }
+export interface PreferencesState extends PreferencesDataStorageType {
+    setAppLanguage: (value: string) => void;
+    setPrimaryLanguage: (value: string) => void;
+    setDisableAutoPlay: (value: boolean) => void;
+    setDisableHaptics: (value: boolean) => void;
+    setDisableTranslation: (value: boolean) => void;
+    setTranslationLanguage: (value: string) => void;
+    setPreferences: (value: PreferencesDataStorageType) => void;
+    loadPreferencesFromStorage: () => void;
+    removePreferencesFromStorage: () => void;
 }
+
+export const usePreferencesStore = create<PreferencesState>((set) => ({
+    language: {
+        appLanguage: storage.getString(storageKey.appLanguage) || 'en',
+        primaryLanguage: storage.getString(storageKey.primaryLanguage) || 'en',
+    },
+    content: {
+        disableAutoplay: storage.getBoolean(storageKey.autoplay) || false,
+        disableHaptics: storage.getBoolean(storageKey.haptics) || false,
+        disableTranslation: storage.getBoolean(storageKey.translation) || false,
+        translationLanguage: storage.getString(storageKey.translationLanguage) || 'en',
+    },
+    setAppLanguage: (value: string) => {
+        storage.set(storageKey.appLanguage, value);
+        set((state) => ({
+            language: {
+                ...state.language,
+                appLanguage: value,
+            },
+        }));
+    },
+    setPrimaryLanguage: (value: string) => {
+        storage.set(storageKey.primaryLanguage, value);
+        set((state) => ({
+            language: {
+                ...state.language,
+                primaryLanguage: value,
+            },
+        }));
+    },
+    setDisableAutoPlay: (value: boolean) => {
+        storage.set(storageKey.autoplay, value);
+        set((state) => ({
+            content: {
+                ...state.content,
+                disableAutoplay: value,
+            },
+        }));
+    },
+    setDisableHaptics: (value: boolean) => {
+        storage.set(storageKey.haptics, value);
+        set((state) => ({
+            content: {
+                ...state.content,
+                disableHaptics: value,
+            },
+        }));
+    },
+    setDisableTranslation: (value: boolean) => {
+        storage.set(storageKey.translation, value);
+        set((state) => ({
+            content: {
+                ...state.content,
+                disableTranslation: value,
+            },
+        }));
+    },
+    setTranslationLanguage: (value: string) => {
+        storage.set(storageKey.translationLanguage, value);
+        set((state) => ({
+            content: {
+                ...state.content,
+                translationLanguage: value,
+            },
+        }));
+    },
+    setPreferences: (value: PreferencesDataStorageType) => {
+        set({
+            language: value.language,
+            content: value.content
+        });
+        storage.set(storageKey.appLanguage, value.language.appLanguage)
+        storage.set(storageKey.primaryLanguage, value.language.primaryLanguage)
+        storage.set(storageKey.haptics, value.content.disableHaptics)
+        storage.set(storageKey.autoplay, value.content.disableAutoplay)
+        storage.set(storageKey.translation, value.content.disableTranslation)
+        storage.set(storageKey.translationLanguage, value.content.translationLanguage)
+
+    },
+    loadPreferencesFromStorage: () => {
+        set({
+            language: {
+                appLanguage: storage.getString(storageKey.appLanguage) || 'en',
+                primaryLanguage: storage.getString(storageKey.primaryLanguage) || 'en',
+            },
+            content: {
+                disableAutoplay: storage.getBoolean(storageKey.autoplay) || false,
+                disableHaptics: storage.getBoolean(storageKey.haptics) || false,
+                disableTranslation: storage.getBoolean(storageKey.translation) || false,
+                translationLanguage: storage.getString(storageKey.translationLanguage) || 'en',
+            },
+        });
+    },
+    removePreferencesFromStorage: () => {
+        storage.delete(storageKey.appLanguage);
+        storage.delete(storageKey.primaryLanguage);
+        storage.delete(storageKey.autoplay);
+        storage.delete(storageKey.haptics);
+        storage.delete(storageKey.translation);
+        storage.delete(storageKey.translationLanguage);
+
+        set({
+            language: {
+                appLanguage: 'en',
+                primaryLanguage: 'en',
+            },
+            content: {
+                disableAutoplay: false,
+                disableHaptics: false,
+                disableTranslation: false,
+                translationLanguage: 'en',
+            },
+        });
+    },
+}));
