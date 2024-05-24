d126be9a1400f286a4339cc9cf095721484de5d5
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-05-24 14:08:56 -0300

    add: zustand to store control

--- src/contexts/Persisted/persistedUser.ts
+++ src/contexts/Persisted/persistedUser.ts
@@ -1,72 +1,107 @@
-import { userReciveDataProps } from "../../components/user_show/user_show-types"
-import { storage } from "../../store"
-import { storageKeys } from "./storageKeys"
+import create from 'zustand';
+import { storage } from '../../store';
+import { storageKeys } from './storageKeys';
+import { UserRootProps, userReciveDataProps } from '../../components/user_show/user_show-types';
+import { UserDataType } from './types';
+const storageKey = storageKeys().user;
 
-const storageKey = storageKeys().user
-
-export class persistedUserClass {
-    public id: Number
-    public username: String
-    public verifyed: Boolean
-    public profile_picture: {
-        small_resolution: String
-        tiny_resolution: String
-    }
-
-    constructor({
-        id,
-        username,
-        verifyed,
-        profile_picture
-    }: userReciveDataProps) {
-        this.id = id
-        this.username = username
-        this.verifyed = verifyed
-        this.profile_picture = profile_picture
-    }
-
-    public setId(value: number) {
-        this.id = value
-        storage.set(storageKey.id, value)
-    }
-    public setUsername(value: string) {
-        this.username = value
-        storage.set(storageKey.username, value)
-    }
-    public setVerifyed(value: boolean) {
-        this.verifyed = value
-        storage.set(storageKey.verifyed, value)
-    }
-    public setProfilePicture(value: {
-        small_resolution: String
-        tiny_resolution: String
-    } ) {
-        this.profile_picture = value
-        storage.set(storageKey.id, JSON.stringify(value))
-    }
-
-    public storeUser(value: userReciveDataProps) {
-        this.setId(value.id)
-        this.setUsername(value.username)
-        this.setVerifyed(value.verifyed)
-        this.setProfilePicture(value.profile_picture)
-    }
-
-    public loadUserFromStorage() {
-        const id = storage.getNumber(storageKey.id)
-        const username = storage.getString(storageKey.username)
-        const verifyed = storage.getBoolean(storageKey.verifyed)
-        const profile_picture = storage.getString(storageKey.profile_picture)
-        if(id) this.id = id
-        if(username) this.username = username
-        if(verifyed) this.verifyed = verifyed
-        if(profile_picture) this.profile_picture = JSON.parse(profile_picture)
-    }
-
-    public removeUserFromStorage() {
-        storage.delete(storageKey.id)
-        storage.delete(storageKey.username)
-        storage.delete(storageKey.verifyed)
-        storage.delete(storageKey.profile_picture)
-    }
+export interface UserState {
+    id: number;
+    username: string;
+    description: string;
+    verified: boolean;
+    profile_picture: {
+        small_resolution: string;
+        tiny_resolution: string;
+    };
+    setId: (value: number) => void;
+    setUsername: (value: string) => void;
+    setDescription: (value: string) => void;
+    setVerified: (value: boolean) => void;
+    setProfilePicture: (value: {
+        small_resolution: string;
+        tiny_resolution: string;
+    }) => void;
+    setUser: (value: UserDataType) => void
+    loadUserFromStorage: () => void;
+    removeUserFromStorage: () => void;
 }
+
+export const useUserStore = create<UserState>((set) => ({
+    id: storage.getNumber(storageKey.id) || 0,
+    username: storage.getString(storageKey.username) || '',
+    description: storage.getString(storageKey.description) || '',
+    verified: storage.getBoolean(storageKey.verifyed) || false,
+    profile_picture: {
+        small_resolution: storage.getString(storageKey.profile_picture.small) || '',
+        tiny_resolution: storage.getString(storageKey.profile_picture.tiny) || ''
+    },
+
+    setId: (value: number) => {
+        storage.set(storageKey.id, value);
+        set({ id: value });
+    },
+    setUsername: (value: string) => {
+        storage.set(storageKey.username, value);
+        set({ username: value });
+    },
+    setDescription: (value: string) => {
+        storage.set(storageKey.description, value);
+        set({ description: value });
+    },
+    setVerified: (value: boolean) => {
+        storage.set(storageKey.verifyed, value);
+        set({ verified: value });
+    },
+    setProfilePicture: (value: {
+        small_resolution: string;
+        tiny_resolution: string;
+    }) => {
+        storage.set(storageKey.profile_picture.small, value.small_resolution)
+        storage.set(storageKey.profile_picture.tiny, value.tiny_resolution)
+        set({ profile_picture: value });
+    },
+    setUser: (value: UserDataType) => {
+        set({
+            id: value.id,
+            username: value.username,
+            description: value.description,
+            verified: value.verifyed,
+            profile_picture: value.profile_picture
+        })
+        storage.set(storageKey.id, value.id);
+        storage.set(storageKey.username, value.username);
+        storage.set(storageKey.description, value.description);
+        storage.set(storageKey.verifyed, value.verifyed);
+        storage.set(storageKey.profile_picture.small, value.profile_picture.small_resolution)
+        storage.set(storageKey.profile_picture.tiny, value.profile_picture.tiny_resolution)
+    },
+    loadUserFromStorage: () => {
+        set({
+            id: storage.getNumber(storageKey.id) || 0,
+            username: storage.getString(storageKey.username) || '',
+            description: storage.getString(storageKey.description) || '',
+            verified: storage.getBoolean(storageKey.verifyed) || false,
+            profile_picture: {
+                small_resolution: storage.getString(storageKey.profile_picture.small) || '',
+                tiny_resolution: storage.getString(storageKey.profile_picture.tiny) || ''
+            },
+        });
+    },
+    removeUserFromStorage: () => {
+        storage.delete(storageKey.id);
+        storage.delete(storageKey.username);
+        storage.delete(storageKey.description);
+        storage.delete(storageKey.verifyed);
+        storage.delete(storageKey.profile_picture.small)
+        storage.delete(storageKey.profile_picture.tiny)
+
+        set({
+            id: 0,
+            username: '',
+            description: '',
+            verified: false,
+            profile_picture: { small_resolution: '', tiny_resolution: '' },
+        });
+    },
+}));
