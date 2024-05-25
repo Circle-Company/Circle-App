ea5edbab82940c4893fe9c5c511e955f384c3f98
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-05-24 22:11:53 -0300

    add: setUserClass with user data from AuthContext

--- src/contexts/Persisted/index.tsx
+++ src/contexts/Persisted/index.tsx
@@ -3,6 +3,7 @@ import { SessionDataType } from './types'
 import { useUserStore } from './persistedUser'
 import { useAccountStore } from './persistedAccount'
 import { usePreferencesStore } from './persistedPreferences'
+import AuthContext from '../auth'
 
 type PersistedProviderProps = { children: React.ReactNode }
 export type PersistedContextData = { session: SessionDataType }
@@ -10,11 +11,23 @@ export type PersistedContextData = { session: SessionDataType }
 const PersistedContext = React.createContext<PersistedContextData>({} as PersistedContextData)
 
 export function Provider({ children }: PersistedProviderProps) {
+    const { user } = React.useContext(AuthContext)
+
     const [ jwtToken, setJwtToken ] = React.useState<string | null>(null)
 
     const sessionUser = useUserStore()
     const sessionAccount = useAccountStore()
     const sessionPreferences = usePreferencesStore()
+
+    React.useEffect(() => {
+        if(user) sessionUser.setUser({
+            id: user.id,
+            username: user.username,
+            description: user.description? user.description : '',
+            verifyed: user.verifyed,
+            profile_picture: { small_resolution: '', tiny_resolution: user.profile_picture.tiny_resolution}
+        })
+    }, [user])
 
     const contextValue: any = {
         session: {
