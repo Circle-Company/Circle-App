1981bbb2fded921ae2a41bd8b8496847321b5eac
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-05-25 23:45:38 -0300

    remove: app cookie

--- src/config/metadata.tsx
+++ src/config/metadata.tsx
@@ -2,6 +2,5 @@ export default {
     APPLICATION_NAME: 'Circle App',
     APPLICATION_SHORT_NAME: 'Circle',
     APPLICATION_DESCRIPTION: 'Create moments and share memories',
-    COOKIE_NAME: 'circle-app-token',
     ORGANIZATION_NAME: 'Circle Company',
 }
