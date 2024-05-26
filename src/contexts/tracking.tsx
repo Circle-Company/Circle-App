5f4ba054563f1454a272b14df50d479b94bd4cc1
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-05-25 23:48:59 -0300

    add: mixpanel inicializer config

--- src/contexts/tracking.tsx
+++ src/contexts/tracking.tsx
@@ -2,7 +2,9 @@ import React from "react"
 import { getTrackingStatus, requestTrackingPermission, TrackingStatus} from 'react-native-tracking-transparency'
 import { Mixpanel, MixpanelType} from "mixpanel-react-native"
 import getDeviceInfo from "../services/deviceInfo"
-import AuthContext from "./auth";
+import AuthContext from "./auth"
+import config from "../config"
+
 type TrackingProviderProps = { children: React.ReactNode }
 export type TrackingContextsData = {
     mixpanel: MixpanelType,
@@ -11,11 +13,17 @@ export type TrackingContextsData = {
 const TrackingContext = React.createContext<TrackingContextsData>({} as TrackingContextsData)
 
 export function TrackingProvider({children}: TrackingProviderProps) {
-    const { user } = React.useContext(AuthContext)
+    const { sessionData } = React.useContext(AuthContext)
 
-    const trackAutomaticEvents = false;
-    const mixpanel = new Mixpanel("Your Token", trackAutomaticEvents);
-    mixpanel.init(true);
+    const trackAutomaticEvents = false
+    const mixpanel = new Mixpanel(config.MIXPANEL_KEY, trackAutomaticEvents);
+    mixpanel.init(true)
+
+
+    function newSign(){
+        mixpanel.identify(sessionData.user.id.toString())
+        mixpanel.getPeople().set("@username", sessionData.user.username)
+    }
 
     const contextValue: any = { mixpanel }
 
