d63a9657ac03be07a000faa97ec558691be3fe5f
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-06-06 01:30:23 -0300

    add: generate random id number

---
+++ src/algorithms/generateRandomId.ts
@@ -0,0 +1,12 @@
+export function generateRandomId(length: number): number {
+    // Gerar um número inteiro aleatório de 20 caracteres
+    let randomNumber = '';
+    const characters = '0123456789';
+    const charactersLength = characters.length;
+  
+    for (let i = 0; i < length; i++) {
+      randomNumber += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+  
+    return Number(randomNumber)
+}
