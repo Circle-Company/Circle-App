203a8b18789b6873a0e5ca33e3721cba420a9f50
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-06-06 01:36:25 -0300

    add ignore log

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import {AppRegistry, LogBox} from 'react-native'
 
 LogBox.ignoreLogs([
   "[react-native-gesture-handler] Seems like you\'re using an old API with gesture components, check out new Gestures system!",
+  "[DEPRECATED] Default export is deprecated. Instead use `import { create } from 'zustand'`."
 ])
 LogBox.ignoreLogs(["new NativeEventEmitter()"]);
 
