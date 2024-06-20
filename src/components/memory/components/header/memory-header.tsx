56da6f3192eb5e82ad261bf5c110d9a7a576de79
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-06-20 11:16:42 -0300

    refactor: remove spaces

--- src/components/memory/components/header/memory-header.tsx
+++ src/components/memory/components/header/memory-header.tsx
@@ -10,7 +10,7 @@ export default function header ({children}: MemoryHeaderProps) {
         alignItems: 'center',
         width: sizes.screens.width,
         paddingHorizontal: sizes.margins['3sm'],
-        paddingVertical: sizes.paddings["1sm"]*0.8,
+        paddingVertical: sizes.paddings["1sm"]*0.8
     }
     
     return <View style={container}>{children}</View>
