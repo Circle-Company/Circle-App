1e0413328fb12d623ff58e98e6169b0a3491add8
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-06-20 11:06:15 -0300

    refactor: button text message

--- src/components/headers/memories/memories-list_moments-header_right.tsx
+++ src/components/headers/memories/memories-list_moments-header_right.tsx
@@ -25,7 +25,7 @@ export default function MemoriesListMomentsHeaderRight({user_id}: {user_id: numb
         <View style={container}>
             <ViewMorebutton
                 action={() => {navigation.navigate('MemoriesNavigator', { screen: 'EditMemory'})}}
-                text={t('Add')}
+                text={t('Edit')}
                 scale={1.2}
                 fontFamily={fonts .family.Semibold}
             />
