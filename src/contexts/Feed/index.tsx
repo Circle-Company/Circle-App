386a2e399cf39bbfbfb9b6af544cfb6c96b047fc
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-06-22 14:50:50 -0300

    add: feed pagination + send interactions to api

--- src/contexts/Feed/index.tsx
+++ src/contexts/Feed/index.tsx
@@ -1,14 +1,25 @@
 import React from "react"
-
-import moments from '../../data/moment.json'
 import {FeedProviderProps, MomentProps } from './types'
 import { Animated } from "react-native"
-import MomentContext from "../../components/moment/context"
-import { MomentDataProps } from "../../components/moment/context/types"
+import { MomentDataProps, MomentUserActionsProps, TagProps } from "../../components/moment/context/types"
 import api from "../../services/api"
 import PersistedContext from "../Persisted"
-import { CommentObject } from "../../components/comment/comments-types"
+import { LanguagesCodesType } from "../../locales/LanguageTypes"
+import { useTimer } from "../../lib/hooks/useTimer"
 
+export type InteractionProps = {
+    id: number,
+    tags: TagProps[],
+    duration: number,
+    type: "IMAGE" | "VIDEO",
+    language: LanguagesCodesType,
+    interaction: MomentUserActionsProps
+}
+export type ChunkInteractionsProps = {
+    length: number,
+    period: number,
+    data: InteractionProps[]
+}
 type FeedContextsData = {
     feedData: MomentProps[]
     loadingFeed: boolean
@@ -19,8 +30,11 @@ type FeedContextsData = {
     allCommentsEnabled: boolean
     enableScrollFeed: boolean
     scrollOffset: Animated.Value
-    showKeyboard: boolean
+    showKeyboard: boolean,
+    currentChunkIds: Array<number>
     getFeed: () => Promise<void>
+    reloadFeed: () => Promise<void>
+    setChunkInteractionsFunc: (value: InteractionProps) => void,
     setShowKeyboard: React.Dispatch<React.SetStateAction<boolean>>
     setAllCommentsEnabled: React.Dispatch<React.SetStateAction<boolean>>
     setFocusedItemIndex: React.Dispatch<React.SetStateAction<number>>
@@ -38,18 +52,61 @@ export function FeedProvider({children}: FeedProviderProps) {
     const [ focusedItemId, setFocusedItemId] = React.useState<null | number>(null)
     const [ feedData, setFeedData ] = React.useState<MomentProps[]>([])
     const [ commentEnabled, setCommentEnabled ] = React.useState(false)
+    const [ currentChunkIds, setCurrentChunksIds ] = React.useState<Array<number>>([])
     const [ enableScrollFeed, setEnableScrollFeed ] = React.useState(true)
     const [ allCommentsEnabled, setAllCommentsEnabled ] = React.useState(false)
     const [ scrollOffset ] = React.useState(new Animated.Value(0))
     const [ showKeyboard, setShowKeyboard ] = React.useState(false)
     const [ focusedMoment, setFocusedMoment ] = React.useState<MomentDataProps>()
     const [ sendedComments, setSendedComments ] = React.useState<any>()
+    const [ chunkInteractions, setChunkInteractions ] = React.useState<InteractionProps[]>([] as InteractionProps[])
+    const [period, setPeriod] = React.useState(0)
 
-    async function getFeed() {
-        await api.post(`/moment/get-feed`, { 
-            user_id: session.user.id
+    const handleTick = () => {
+        setPeriod(prevCount => prevCount + 1);
+    };
+
+    const [resetTimer] = useTimer(1000, handleTick);
+
+    function setChunkInteractionsFunc(value: InteractionProps) {
+        const valueToInsert = chunkInteractions.concat(value)
+        setChunkInteractions(valueToInsert)
+    }
+
+    async function reloadFeed() {
+        setEnableScrollFeed(false)
+        console.log('dataaaaaaaaaaaa: ', chunkInteractions)
+        await api.post(`/moment/get-feed`, {
+            user_id: session.user.id,
+            period,
+            length: chunkInteractions.length,
+            data: chunkInteractions
         })
-        .then(function (response) { setFeedData(response.data) })
+        .then(function (response) {
+            const postsIds = response.data.map((moment: any) => moment.id)
+            setCurrentChunksIds(postsIds)
+            setFeedData(response.data)
+            setEnableScrollFeed(true)
+            resetTimer()
+        })
+    }
+    async function getFeed() {
+        setEnableScrollFeed(false)
+        console.log('dataaaaaaaaaaaa: ', chunkInteractions)
+        await api.post(`/moment/get-feed`, {
+            user_id: session.user.id,
+            period,
+            length: chunkInteractions.length,
+            data: chunkInteractions
+        })
+        .then(function (response) {
+            if(feedData.length > 0) setFeedData([...feedData , ...response.data])
+            else setFeedData(response.data)
+            const postsIds = response.data.map((moment: any) => moment.id)
+            setCurrentChunksIds(postsIds)
+            setEnableScrollFeed(true)
+            resetTimer()
+        })
         .catch(function (error) { console.log(error)})  
     }
 
@@ -84,13 +141,16 @@ export function FeedProvider({children}: FeedProviderProps) {
         showKeyboard,
         focusedMoment,
         sendedComments,
+        currentChunkIds,
         setFocusedItemIndex,
         setFocusedItemId,
         setCommentEnabled,
         setAllCommentsEnabled,
         setFocusedMoment,
         setShowKeyboard,
-        getFeed
+        getFeed,
+        reloadFeed,
+        setChunkInteractionsFunc
     }
 
     return <FeedContext.Provider value={feedContextValue}>{children}</FeedContext.Provider>
