4bed3fd80b8a56dd1e1c1f5daee975d574faf718
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-06-22 15:49:11 -0300

    add: privacy policy + terms of service + community guidelines webview

--- src/pages/app/Settings/CommunityGuidelines.tsx
+++ src/pages/app/Settings/CommunityGuidelines.tsx
@@ -5,7 +5,7 @@ import ColorTheme, {colors} from '../../../layout/constants/colors'
 import { WebView } from 'react-native-webview'
 const WindowWidth = Dimensions.get('window').width
 
-export default function SettingsPrivacyPolicy() {
+export default function SettingsCommunityGuidelines() {
 
   const isDarkMode = useColorScheme() === 'dark'
   const container = {
@@ -16,7 +16,7 @@ export default function SettingsPrivacyPolicy() {
   return (
     <View style={container}>
       <StatusBar barStyle={isDarkMode? 'light-content' : 'dark-content'} backgroundColor={isDarkMode? colors.gray.black.toString(): colors.gray.white.toString()}/>
-      <WebView source={{ uri: 'https://circle-company.github.io/Circle-Website/privacy-policy' }} style={{ flex: 1 }} />
+      <WebView source={{ uri: 'https://circle-company.github.io/Circle-Website/community-guidelines' }} style={{ flex: 1 }} />
     </View>
   )
 }
