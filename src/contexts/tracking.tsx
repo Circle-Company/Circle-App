a8433665b018c2fb3d44273442f45e3684316c38
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-07-09 00:30:05 -0300

    add: track sign functions

--- src/contexts/tracking.tsx
+++ src/contexts/tracking.tsx
@@ -4,11 +4,19 @@ import { Mixpanel, MixpanelType} from "mixpanel-react-native"
 import getDeviceInfo from "../services/deviceInfo"
 import AuthContext from "./auth"
 import config from "../config"
+import { UserDataReturnsType } from "./Persisted/types"
 
+type trackSignProps = {
+    user: UserDataReturnsType,
+    signType: 'SIGN-IN' | 'SIGN-UP' | 'SIGN-OUT'
+}
 type TrackingProviderProps = { children: React.ReactNode }
 export type TrackingContextsData = {
-    mixpanel: MixpanelType,
+    mixpanel: MixpanelType
+    trackSign: ({user, signType}: trackSignProps) => void
 }
+
+
 
 const TrackingContext = React.createContext<TrackingContextsData>({} as TrackingContextsData)
 
@@ -17,15 +25,30 @@ export function TrackingProvider({children}: TrackingProviderProps) {
 
     const trackAutomaticEvents = false
     const mixpanel = new Mixpanel(config.MIXPANEL_KEY, trackAutomaticEvents);
-    mixpanel.init(true)
+    mixpanel.init() 
+
+    React.useEffect(() => {
+        if(sessionData.user){
+            mixpanel.identify(sessionData.user.id.toString())
+            mixpanel.getPeople().set({
+                "username": sessionData.user.username,
+                "name": sessionData.user.name,
+                "verification": sessionData.user.verifyed
+            })
+        }
+    }, [])
 
 
-    function newSign(){
-        mixpanel.identify(sessionData.user.id.toString())
-        mixpanel.getPeople().set("@username", sessionData.user.username)
+    function trackSign({user, signType}: trackSignProps){
+        if(signType == 'SIGN-IN') mixpanel.track('Sign In', { platform: 'mobile' })
+        if(signType == 'SIGN-UP') mixpanel.track('Sign Up', { platform: 'mobile' })
+        if(signType == 'SIGN-OUT') mixpanel.track('Sign Out', { platform: 'mobile' })
     }
 
-    const contextValue: any = { mixpanel }
+    const contextValue: TrackingContextsData = {
+        mixpanel,
+        trackSign
+    }
 
     return (
         <TrackingContext.Provider value={contextValue}>
