a5b74596ec309e9b118de327497df4af1fc346ea
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-07-12 21:01:38 -0300

    fix: api endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,12 @@
-import axios from 'axios'
-import config from '../config'
+import axios from "axios"
+import config from "../config"
+
+const PATH = config.PRODUCTION
+    ? `http://${config.CIRCLE_SYSTEM_API}/v${config.API_VERSION}`
+    : `http://192.168.15.12:3000/v${config.API_VERSION}`
 
 const api = axios.create({
-    baseURL: `http://${config.CIRCLE_SYSTEM_API}/v${config.API_VERSION}`
-});
+    baseURL: PATH,
+})
 
-export default api;
+export default api
