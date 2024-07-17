170880a76ca7f678973783fcb29485ab702b6406
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-07-16 23:37:01 -0300

    refactor: eslint improve legibility

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,45 +1,43 @@
-import emails from './emails'
-import metadata from './metadata'
 import {
-    APP_VERSION,
-    NODE_ENV,
-    DEBUG,
-    MIXPANEL_KEY,
-    LOG_LEVEL,
-    LOG_DEBUG,
     API_VERSION,
-    CIRCLE_SYSTEM_API
+    APP_VERSION,
+    CIRCLE_SYSTEM_API,
+    DEBUG,
+    LOG_DEBUG,
+    LOG_LEVEL,
+    MIXPANEL_KEY,
+    NODE_ENV,
 } from "@env"
+import emails from "./emails"
+import metadata from "./metadata"
 
 const environment = {
     APP_VERSION: APP_VERSION,
     NODE_ENV: NODE_ENV,
-    DEBUG: NODE_ENV !== 'production' && DEBUG,
-    TEST: NODE_ENV === 'test',
-    PRODUCTION: NODE_ENV === 'production',
+    DEBUG: NODE_ENV !== "production" && DEBUG,
+    TEST: NODE_ENV === "test",
+    PRODUCTION: NODE_ENV === "production",
 }
 
 const api = {
     API_VERSION: API_VERSION,
-    CIRCLE_SYSTEM_API: CIRCLE_SYSTEM_API
+    CIRCLE_SYSTEM_API: CIRCLE_SYSTEM_API,
 }
 
 const analytics = {
-    MIXPANEL_KEY: MIXPANEL_KEY
+    MIXPANEL_KEY: MIXPANEL_KEY,
 }
 
 const log = {
     LOG_LEVEL: LOG_LEVEL,
-    LOG_DEBUG: LOG_DEBUG
+    LOG_DEBUG: LOG_DEBUG,
 }
 
 const required = {
-    APP_VERSION: APP_VERSION,
     NODE_ENV: NODE_ENV,
     MIXPANEL_KEY: MIXPANEL_KEY,
-    APPLICATION_NAME: metadata.APPLICATION_NAME
+    APPLICATION_NAME: metadata.APPLICATION_NAME,
 }
-
 
 const options = {
     SUPPORT_URL: emails.SUPPORT_LINK,
@@ -47,22 +45,21 @@ const options = {
     APPLICATION_SHORT_NAME: metadata.APPLICATION_SHORT_NAME,
     APPLICATION_DESCRIPTION: metadata.APPLICATION_DESCRIPTION,
     ORGANIZATION_URL: emails.ORGANIZATION_LINK,
-    ORGANIZATION_NAME: metadata.ORGANIZATION_NAME
+    ORGANIZATION_NAME: metadata.ORGANIZATION_NAME,
 }
 
-  // Check if all required configs are present
+// Check if all required configs are present
 Object.entries(required).map((entry) => {
     if (!entry[1]) {
-      throw new Error(`ERROR: "${entry[0]}" env variable is missing.`)
+        throw new Error(`ERROR: "${entry[0]}" env variable is missing.`)
     }
     return entry
 })
-
 
 export default {
     ...api,
     ...environment,
     ...log,
     ...options,
-    ...analytics
-  }
+    ...analytics,
+}
