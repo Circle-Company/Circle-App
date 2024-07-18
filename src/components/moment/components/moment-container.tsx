8fd66b7131ae014ecd04acb940eace52cdf2ee21
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-07-18 02:57:38 -0300

    refactor: eslint refactor

--- src/components/moment/components/moment-container.tsx
+++ src/components/moment/components/moment-container.tsx
@@ -1,112 +1,105 @@
-import React from "react"
-import { View, Pressable, Text} from "react-native"
-import ColorTheme, { colors } from "../../../layout/constants/colors"
-import { MidiaRender } from "../../midia_render"
 import { useNavigation } from "@react-navigation/native"
-import MomentContext from "../context"
+import React from "react"
+import { View } from "react-native"
+import Reanimated, { FadeIn, FadeOut } from "react-native-reanimated"
 import FeedContext from "../../../contexts/Feed"
-import { UserShow } from "../../user_show"
-import { MomentContainerProps } from "../moment-types"
-import Reanimated, { FadeIn, FadeInDown, FadeOut } from "react-native-reanimated"
-import { Animated } from "react-native"
-import LikeIcon from '../../../assets/icons/svgs/heart.svg'
-import { opacity } from "react-native-reanimated/lib/typescript/reanimated2/Colors"
+import ColorTheme from "../../../layout/constants/colors"
 import DoubleTapPressable from "../../general/double-tap-pressable"
+import { MidiaRender } from "../../midia_render"
+import { UserShow } from "../../user_show"
+import MomentContext from "../context"
+import { MomentContainerProps } from "../moment-types"
 export default function Container({
     children,
     contentRender,
-    blur_color = String(ColorTheme().background),
-    blurRadius = 35,
     isFocused = true,
-    fromFullMomentScreen = false
-
+    fromFullMomentScreen = false,
 }: MomentContainerProps) {
-    const { momentData, momentUserActions, momentSize, momentOptions } = React.useContext(MomentContext)
-    const { commentEnabled, setFocusedMoment, setFocusedItemId} = React.useContext(FeedContext)
-    const [isOnAnimation, setIsOnAnimation] = React.useState(false)
-    var animatedScaleIcon = React.useRef(new Animated.Value(1)).current
-    var animatedOpacityIcon = React.useRef(new Animated.Value(1)).current
-
+    const { momentData, momentUserActions, momentSize, momentOptions } =
+        React.useContext(MomentContext)
+    const { commentEnabled, setFocusedMoment, setFocusedItemId } = React.useContext(FeedContext)
     const navigation = useNavigation()
-    
+
     const container: any = {
         ...momentSize,
-        overflow: 'hidden',
+        overflow: "hidden",
         backgroundColor: ColorTheme().backgroundDisabled,
     }
     const content_container: any = {
         flex: 1,
-        position: 'absolute',
+        position: "absolute",
         width: momentSize.width,
         height: momentSize.height,
-        zIndex: 0
-
+        zIndex: 0,
     }
 
     const tiny_container: any = {
         width: momentSize.width * 0.31,
         height: momentSize.height * 0.31,
-        position: 'absolute',
-        alignItems: 'flex-start',
-        flexDirection: 'row',
+        position: "absolute",
+        alignItems: "flex-start",
+        flexDirection: "row",
         top: 190,
         left: 120,
         flex: 1,
         zIndex: 1,
-        transform: [{scale: 3}]
-    }
-
-    const icon_container: any = {
-        transform: [{ scale: animatedScaleIcon }],
-        opacity: animatedOpacityIcon,
-        alignItems: 'center',
-        justifyContent: 'center',
+        transform: [{ scale: 3 }],
     }
 
     async function handleDoublePress() {
-        momentUserActions.handleLikeButtonPressed({})         
+        momentUserActions.handleLikeButtonPressed({})
     }
 
     async function handleSinglePress() {
-        if(!commentEnabled && momentOptions.isFeed) {
-            if(!fromFullMomentScreen && isFocused) {
+        if (!commentEnabled && momentOptions.isFeed) {
+            if (!fromFullMomentScreen && isFocused) {
                 momentUserActions.setClickIntoMoment(true)
                 setFocusedMoment(momentData)
                 setFocusedItemId(Number(momentData.id))
             }
-            navigation.navigate('MomentNavigator', { screen: 'DetailScreen' })
+            navigation.navigate("MomentNavigator", { screen: "DetailScreen" })
         }
-
     }
 
     return (
         <View style={container}>
             <View style={content_container}>
-            <DoubleTapPressable
-                onSingleTap={handleSinglePress}
-                onDoubleTap={handleDoublePress}
-            >
-                <MidiaRender.Root data={contentRender} content_sizes={momentSize}>
-                <MidiaRender.RenderImage isFeed={momentOptions.isFeed} enableBlur={true} blur={!isFocused}/>
-                </MidiaRender.Root>  
-            </DoubleTapPressable>         
+                <DoubleTapPressable onSingleTap={handleSinglePress} onDoubleTap={handleDoublePress}>
+                    <MidiaRender.Root data={contentRender} content_sizes={momentSize}>
+                        <MidiaRender.RenderImage
+                            isFeed={momentOptions.isFeed}
+                            enableBlur={true}
+                            blur={!isFocused}
+                        />
+                    </MidiaRender.Root>
+                </DoubleTapPressable>
             </View>
-            {!commentEnabled? 
-                    isFocused? children : null               
-                    :
-                        <Reanimated.View style={tiny_container} entering={FadeIn.delay(300).duration(200)} exiting={FadeOut.duration(100)}>
-                            <UserShow.Root data={momentData.user}>
-                                <View style={{top: 1}}>
-                                    <UserShow.ProfilePicture
-                                    disableAnalytics={true}
-                                    pictureDimensions={{width: 15, height: 15}}
-                                    />  
-                                </View>
-                                <UserShow.Username scale={0.8} disableAnalytics={true} margin={0} truncatedSize={8}/>
-                            </UserShow.Root>
-                        </Reanimated.View>
-                    }
+            {!commentEnabled ? (
+                isFocused ? (
+                    children
+                ) : null
+            ) : (
+                <Reanimated.View
+                    style={tiny_container}
+                    entering={FadeIn.delay(300).duration(200)}
+                    exiting={FadeOut.duration(100)}
+                >
+                    <UserShow.Root data={momentData.user}>
+                        <View style={{ top: 1 }}>
+                            <UserShow.ProfilePicture
+                                disableAnalytics={true}
+                                pictureDimensions={{ width: 15, height: 15 }}
+                            />
+                        </View>
+                        <UserShow.Username
+                            scale={0.8}
+                            disableAnalytics={true}
+                            margin={0}
+                            truncatedSize={8}
+                        />
+                    </UserShow.Root>
+                </Reanimated.View>
+            )}
         </View>
-
     )
 }
