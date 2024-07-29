3648b3cab56d7dc4040d1d04e022dcdebe937b12
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-07-28 22:56:56 -0300

    fix: notification context firebase init config

--- src/contexts/notification.tsx
+++ src/contexts/notification.tsx
@@ -1,65 +1,38 @@
+import messaging from "@react-native-firebase/messaging"
 import React from "react"
-import { GLobalToast } from "../components/notification/global-toast"
-import { NotificationProps } from "../components/notification/notification-types"
-
+import { Alert } from "react-native"
+import { notification } from "../lib/notifications"
+import PersistedContext from "./Persisted"
 type NotificationProviderProps = { children: React.ReactNode }
-export type NotificationContextData = {
-    lastNotification: NotificationProps
-    newNotificationsNum: number
-    allNotifications: NotificationProps[]
-    showNotification: boolean
-    setReadSocketNotifications(): void
-}
+export type NotificationContextData = {}
+
 const NotificationContext = React.createContext<NotificationContextData>(
     {} as NotificationContextData
 )
 
 export function Provider({ children }: NotificationProviderProps) {
-    const [socketNotifications, setSocketNotifications] = React.useState<NotificationProps[]>([])
-    const [newNotificationsNum, setNewNotificationsNum] = React.useState<number>(0)
-    const [lastNotification, setLastNotification] = React.useState<NotificationProps | any>({})
-    const [notifications, setNotifications] = React.useState([])
-    const [allNotifications, setAllNotifications] = React.useState<any[]>([])
-    const [showNotification, setShowNotification] = React.useState<boolean>(false)
-
-    /**
-     *     React.useEffect(() => {
-        socket.on("new-notification", (notification: any) => {
-            setSocketNotifications((prev: any):any => [...prev, notification]);
-            setLastNotification(notification)
-            setShowNotification(false)
-            setShowNotification(true)
-        })
-    }, [socket])
-     */
-
+    const { session } = React.useContext(PersistedContext)
     React.useEffect(() => {
-        //setNewNotificationsNum(socketNotifications.length)
-        if (!lastNotification)
-            setLastNotification(socketNotifications[socketNotifications.length - 1])
-        //setAllNotifications([...socketNotifications, ...notifications])
-        setAllNotifications([...notifications])
-    }, [socketNotifications, notifications])
+        async function get() {
+            let token = await messaging().getToken()
+            session.account.setFirebasePushToken(token.toString())
+            notification.registerPushToken({ userId: session.user.id, token })
+        }
+        get()
+    }, [])
 
-    const setReadSocketNotifications = () => {
-        //setNewNotificationsNum(0)
-        //setSocketNotifications([])
-        setLastNotification({})
-    }
+    messaging().onTokenRefresh((token) => {
+        notification.refreshPushToken(token)
+        notification.registerPushToken({ userId: session.user.id, token })
+    })
 
-    return (
-        <NotificationContext.Provider
-            value={{
-                newNotificationsNum,
-                allNotifications,
-                lastNotification,
-                setReadSocketNotifications,
-                showNotification,
-            }}
-        >
-            <GLobalToast showNotification={showNotification} lastNotification={lastNotification} />
-            {children}
-        </NotificationContext.Provider>
-    )
+    messaging().onMessage(async (remoteMessage) => {
+        Alert.alert("New message recieved", JSON.stringify(remoteMessage))
+    })
+
+    messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+        Alert.alert("New message recieved", JSON.stringify(remoteMessage))
+    })
+    return <NotificationContext.Provider value={{}}>{children}</NotificationContext.Provider>
 }
 export default NotificationContext
