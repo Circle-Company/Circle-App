c560f867b71d3b8091c201a777d75cf13ddb9195
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-07-28 22:54:33 -0300

    fix: api endpoint

--- src/services/Api/index.ts
+++ src/services/Api/index.ts
@@ -1,10 +1,10 @@
 import axios from "axios"
 import config from "../../config"
 import { routes as momentRoutes } from "./routes/moment"
+import { routes as notificationRoutes } from "./routes/notification"
 import { routes as userRoutes } from "./routes/user"
 
-const PATH = `http://23.20.95.66:3000/v${config.API_VERSION}`
-
+const PATH = `http://${config.ENDPOINT}/v${config.API_VERSION}`
 const api = axios.create({ baseURL: PATH })
 
 export default api
@@ -14,4 +14,5 @@ export const API = api
 export const apiRoutes = {
     moment: momentRoutes,
     user: userRoutes,
+    notification: notificationRoutes,
 }
