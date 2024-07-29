f4167f6c1da97fda3941c9ed26533ffdb7291245
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-07-28 23:09:18 -0300

    refactor: remove console.log

--- src/services/Api/routes/notification/index.ts
+++ src/services/Api/routes/notification/index.ts
@@ -2,7 +2,6 @@ import api from "../.."
 import { PostTokenProps } from "./types"
 
 async function setToken({ userId, token }: PostTokenProps): Promise<void> {
-    console.log("fetch api setToken", { userId, token })
     await api.post("/notification/token/store", {
         userId,
         token,
