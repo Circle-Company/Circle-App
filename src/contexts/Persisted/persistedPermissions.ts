c5783f2de4c02fbacd023636338bcdd5b06a9eb7
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-07-28 22:56:39 -0300

    add: firebaseMessaging

--- src/contexts/Persisted/persistedPermissions.ts
+++ src/contexts/Persisted/persistedPermissions.ts
@@ -7,6 +7,7 @@ const storageKey = storageKeys().permissions
 
 export interface PermissionsState extends PermissionsData {
     setPostNotifications: (value: boolean) => void
+    setFirebaseMessaging: (value: boolean) => void
     set: (value: PermissionsData) => void
     load: () => void
     remove: () => void
@@ -14,24 +15,36 @@ export interface PermissionsState extends PermissionsData {
 
 export const usePermissionsStore = create<PermissionsState>((set) => ({
     postNotifications: storage.getBoolean(storageKey.postNotifications) || false,
+    firebaseMessaging: storage.getBoolean(storageKey.firebaseMessaging) || false,
 
     setPostNotifications: (value: boolean) => {
         storage.set(storageKey.postNotifications, value)
         set({ postNotifications: value })
     },
+    setFirebaseMessaging: (value: boolean) => {
+        storage.set(storageKey.firebaseMessaging, value)
+        set({ postNotifications: value })
+    },
     set: (value: PermissionsData) => {
-        set({ postNotifications: value.postNotifications })
+        set({
+            postNotifications: value.postNotifications,
+            firebaseMessaging: value.firebaseMessaging,
+        })
         storage.set(storageKey.postNotifications, value.postNotifications)
+        storage.set(storageKey.firebaseMessaging, value.firebaseMessaging)
     },
     load: () => {
         set({
             postNotifications: storage.getBoolean(storageKey.postNotifications) || false,
+            firebaseMessaging: storage.getBoolean(storageKey.firebaseMessaging) || false,
         })
     },
     remove: () => {
         storage.delete(storageKey.postNotifications)
+        storage.delete(storageKey.firebaseMessaging)
         set({
             postNotifications: false,
+            firebaseMessaging: false,
         })
     },
 }))
