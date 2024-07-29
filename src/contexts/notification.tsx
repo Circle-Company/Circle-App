f897376c4c7e9021def0de5a65d9eef8e9cfd634
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-07-28 23:55:06 -0300

    add: request send notification permission

--- src/contexts/notification.tsx
+++ src/contexts/notification.tsx
@@ -1,6 +1,7 @@
 import messaging from "@react-native-firebase/messaging"
 import React from "react"
 import { Alert } from "react-native"
+import { useRequestPermission } from "../lib/hooks/userRequestPermissions"
 import { notification } from "../lib/notifications"
 import PersistedContext from "./Persisted"
 type NotificationProviderProps = { children: React.ReactNode }
@@ -13,12 +14,20 @@ const NotificationContext = React.createContext<NotificationContextData>(
 export function Provider({ children }: NotificationProviderProps) {
     const { session } = React.useContext(PersistedContext)
     React.useEffect(() => {
-        async function get() {
-            let token = await messaging().getToken()
-            session.account.setFirebasePushToken(token.toString())
-            notification.registerPushToken({ userId: session.user.id, token })
+        async function requestUserPermission() {
+            useRequestPermission.postNotifications()
+            const status: number = await messaging().requestPermission()
+            const enabled =
+                status === messaging.AuthorizationStatus.AUTHORIZED ||
+                status === messaging.AuthorizationStatus.PROVISIONAL
+            if (enabled) {
+                let token = await messaging().getToken()
+                console.log("token: ", token)
+                session.account.setFirebasePushToken(token.toString())
+                notification.registerPushToken({ userId: session.user.id, token })
+            }
         }
-        get()
+        requestUserPermission()
     }, [])
 
     messaging().onTokenRefresh((token) => {
