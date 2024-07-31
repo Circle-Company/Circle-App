2d4bf03da1ebfdd170814b1f4f58fd0f9a1d8b44
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-07-31 09:57:02 -0300

    add: show notification toas when receive notification

--- src/contexts/notification.tsx
+++ src/contexts/notification.tsx
@@ -1,9 +1,11 @@
 import messaging from "@react-native-firebase/messaging"
 import React from "react"
-import { Alert } from "react-native"
+import { notify } from "react-native-notificated"
 import { useRequestPermission } from "../lib/hooks/userRequestPermissions"
 import { notification } from "../lib/notifications"
 import PersistedContext from "./Persisted"
+import LanguageContext from "./Preferences/language"
+
 type NotificationProviderProps = { children: React.ReactNode }
 export type NotificationContextData = {}
 
@@ -13,6 +15,7 @@ const NotificationContext = React.createContext<NotificationContextData>(
 
 export function Provider({ children }: NotificationProviderProps) {
     const { session } = React.useContext(PersistedContext)
+    const { t } = React.useContext(LanguageContext)
     React.useEffect(() => {
         async function requestUserPermission() {
             useRequestPermission.postNotifications()
@@ -22,7 +25,6 @@ export function Provider({ children }: NotificationProviderProps) {
                 status === messaging.AuthorizationStatus.PROVISIONAL
             if (enabled) {
                 let token = await messaging().getToken()
-                console.log("token: ", token)
                 session.account.setFirebasePushToken(token.toString())
                 notification.registerPushToken({ userId: session.user.id, token })
             }
@@ -36,11 +38,21 @@ export function Provider({ children }: NotificationProviderProps) {
     })
 
     messaging().onMessage(async (remoteMessage) => {
-        Alert.alert("New message recieved", JSON.stringify(remoteMessage))
+        notify("notification", {
+            params: {
+                title: remoteMessage.notification?.title,
+                description: remoteMessage.notification?.body,
+            },
+        })
     })
 
     messaging().setBackgroundMessageHandler(async (remoteMessage) => {
-        Alert.alert("New message recieved", JSON.stringify(remoteMessage))
+        notify("notification", {
+            params: {
+                title: remoteMessage.notification?.title,
+                description: remoteMessage.notification?.body,
+            },
+        })
     })
     return <NotificationContext.Provider value={{}}>{children}</NotificationContext.Provider>
 }
