9af7013b35732e5784bf63bdc9f4ebeaf0da0798
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-07-31 09:56:28 -0300

    add: notification toast

--- src/contexts/Toast/index.tsx
+++ src/contexts/Toast/index.tsx
@@ -1,9 +1,10 @@
 import React from "react"
-import { createNotifications, MoveUp, MoveDown} from 'react-native-notificated'
-import { Notify} from "react-native-notificated/lib/typescript/types"
-import { DefaultVariants} from "react-native-notificated/lib/typescript/defaultConfig/types"
+import { MoveDown, createNotifications } from "react-native-notificated"
+import { DefaultVariants } from "react-native-notificated/lib/typescript/defaultConfig/types"
+import { Notify } from "react-native-notificated/lib/typescript/types"
 import sizes from "../../layout/constants/sizes"
-import { Toast as StandartToast  } from "./CustomToast/standart"
+import { Toast as NotificationToast } from "./CustomToast/notification"
+import { Toast as StandartToast } from "./CustomToast/standart"
 import { Toast as TinyToast } from "./CustomToast/tiny"
 type ToastProviderProps = { children: React.ReactNode }
 export type ToastContextsData = {
@@ -12,8 +13,8 @@ export type ToastContextsData = {
 
 const ToastContext = React.createContext<ToastContextsData>({} as ToastContextsData)
 
-export function Provider({children}: ToastProviderProps) {
-    const {NotificationsProvider} = createNotifications({
+export function Provider({ children }: ToastProviderProps) {
+    const { NotificationsProvider } = createNotifications({
         notificationWidth: sizes.toasts.small.width,
         isNotch: false,
         animationConfig: MoveDown,
@@ -21,25 +22,27 @@ export function Provider({children}: ToastProviderProps) {
             toast: {
                 component: StandartToast,
                 config: {
-                  notificationPosition: 'top',
-                  duration: 1500,
+                    notificationPosition: "top",
+                    duration: 1500,
                 },
             },
             tiny: {
                 component: TinyToast,
                 config: {
-                  notificationPosition: 'top',
-                  duration: 1000,
+                    notificationPosition: "top",
+                    duration: 1000,
+                },
+            },
+            notification: {
+                component: NotificationToast,
+                config: {
+                    notificationPosition: "top",
+                    duration: 1000,
                 },
             },
         },
-      })
+    })
 
-    return (
-        <NotificationsProvider>
-            {children}    
-        </NotificationsProvider>
-
-    )
+    return <NotificationsProvider>{children}</NotificationsProvider>
 }
 export default ToastContext
