0bff91cb8b8567aaa2b43115596a90f714c61466
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-08-05 23:22:04 -0300

    add: style profile followers modal

--- src/components/profile/components/statistics/profile-statistics-followers-modal.tsx
+++ src/components/profile/components/statistics/profile-statistics-followers-modal.tsx
@@ -1,15 +1,46 @@
 import React from "react"
-import { Text, View } from "../../../Themed"
+import { View } from "react-native"
+import config from "../../../../config"
+import LanguageContext from "../../../../contexts/Preferences/language"
+import NumberConversor, { formatNumberWithDots } from "../../../../helpers/numberConversor"
+import ColorTheme from "../../../../layout/constants/colors"
+import fonts from "../../../../layout/constants/fonts"
+import { Text } from "../../../Themed"
+import { ProfileReciveDataProps } from "../../profile-types"
 
-type FollowersModelProps = {}
+type FollowersModelProps = {
+    user: ProfileReciveDataProps
+}
 
-export default function FollowersRenderModal({}) {
+export default function FollowersRenderModal({ user }: FollowersModelProps) {
+    const { t } = React.useContext(LanguageContext)
+    const container = {
+        alignItems: "center",
+    }
+    const NumberStyle = {
+        fontSize: fonts.size.title1,
+        fontFamily: fonts.family["Semibold-Italic"],
+    }
+    const DescriptionStyle = {
+        fontSize: fonts.size.body,
+        fontFamily: fonts.family["Semibold-Italic"],
+    }
+
+    const TextStyle = {
+        fontSize: fonts.size.body,
+        fontFamily: fonts.family["Semibold-Italic"],
+        color: ColorTheme().text + "90",
+    }
     return (
         <View>
-            <View>
-                <Text>Total Followers</Text>
-                <Text>1.828.130</Text>
-            </View>
+            <Text style={NumberStyle}>
+                {formatNumberWithDots(user?.statistics?.total_followers_num)}
+                <Text style={DescriptionStyle}> {t("Followers")}</Text>
+            </Text>
+
+            <Text style={TextStyle}>
+                {`@${user?.username} ${t("has a total of")} ${NumberConversor(user?.statistics?.total_followers_num)} ${t("followers throughout his time on")} ${config.APPLICATION_NAME}`}
+            </Text>
         </View>
     )
 }
