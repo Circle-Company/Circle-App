796efc941e377c34ca653cb3b6165e44c18e049f
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-08-15 08:31:14 -0300

    add: react-native-haptic-feedback declaration

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,6 +1,26 @@
 declare module "*.svg" {
-  import React from 'react';
-  import { SvgProps } from "react-native-svg";
-  const content: React.FC<SvgProps>;
-  export default content;
+    import React from "react"
+    import { SvgProps } from "react-native-svg"
+    const content: React.FC<SvgProps>
+    export default content
+}
+
+declare module "react-native-haptic-feedback" {
+    export interface HapticOptions {
+        enableVibrateFallback?: boolean
+        ignoreAndroidSystemSettings?: boolean
+    }
+
+    export type HapticFeedbackTypes =
+        | "impactLight"
+        | "impactMedium"
+        | "impactHeavy"
+        | "notificationSuccess"
+        | "notificationWarning"
+        | "notificationError"
+        | "selection"
+
+    export default class ReactNativeHapticFeedback {
+        static trigger(type: HapticFeedbackTypes, options?: HapticOptions): void
+    }
 }
