02ba3e642c9b5cbc2fc4cf98e16b226da951b645
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-08-16 14:10:25 -0300

    add: eslint refactor

--- src/components/settings/components/list/settings-item-account.tsx
+++ src/components/settings/components/list/settings-item-account.tsx
@@ -1,66 +1,62 @@
-import { Pressable, View, useColorScheme} from "react-native"
-import { SettingsiItemAccountObjectProps } from "../../settings-types"
-import { Text } from "../../../Themed"
 import { useNavigation } from "@react-navigation/native"
-import sizes from "../../../../layout/constants/sizes"
-import ColorTheme, { colors } from "../../../../layout/constants/colors"
-import fonts from "../../../../layout/constants/fonts"
-import ChevronRight from '../../../../assets/icons/svgs/chevron_right.svg'
-import { UserShow } from "../../../user_show"
-import AuthContext from "../../../../contexts/auth"
 import React from "react"
-import TouchID from 'react-native-simple-biometrics'
+import { Pressable, View, useColorScheme } from "react-native"
+import TouchID from "react-native-simple-biometrics"
+import ChevronRight from "../../../../assets/icons/svgs/chevron_right.svg"
 import PersistedContext from "../../../../contexts/Persisted"
 import LanguageContext from "../../../../contexts/Preferences/language"
+import ColorTheme, { colors } from "../../../../layout/constants/colors"
+import fonts from "../../../../layout/constants/fonts"
+import sizes from "../../../../layout/constants/sizes"
+import { Text } from "../../../Themed"
+import { UserShow } from "../../../user_show"
+import { SettingsiItemAccountObjectProps } from "../../settings-types"
 
-export default function item ({
+export default function item({
     name,
     value,
     type,
     navigator,
     navigateTo,
-    secure
+    secure,
 }: SettingsiItemAccountObjectProps) {
-
     const navigation = useNavigation()
     const { t } = React.useContext(LanguageContext)
-    const isDarkMode = useColorScheme() === 'dark'
+    const isDarkMode = useColorScheme() === "dark"
     const { session } = React.useContext(PersistedContext)
 
-    const icon_fill: string = isDarkMode? String(colors.gray.grey_06): String(colors.gray.grey_03)
+    const icon_fill: string = isDarkMode ? String(colors.gray.grey_06) : String(colors.gray.grey_03)
 
     const container: any = {
         width: sizes.screens.width,
         height: sizes.sizes["3md"],
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-        flexDirection: 'row',
+        alignItems: "center",
+        justifyContent: "flex-start",
+        flexDirection: "row",
         borderBottomWidth: 1,
-        borderColor: isDarkMode? colors.gray.grey_08: colors.gray.grey_02
+        borderColor: isDarkMode ? colors.gray.grey_08 : colors.gray.grey_02,
     }
     const container_left: any = {
         paddingLeft: sizes.paddings["1sm"],
-        alignItems: 'flex-start',
-    }    
+        alignItems: "flex-start",
+    }
     const container_right: any = {
-        flexDirection: 'row',
+        flexDirection: "row",
         paddingRight: sizes.paddings["1md"] * 0.7,
-        alignItems: 'center',
-        justifyContent: 'center',
+        alignItems: "center",
+        justifyContent: "center",
         flex: 1,
     }
     const text_style: any = {
-        textAlign: 'right',
+        textAlign: "right",
         fontSize: fonts.size.body,
-        fontFamily: fonts.family.Semibold
-        
+        fontFamily: fonts.family.Semibold,
     }
 
     const value_container: any = {
         flex: 1,
-        alignItems: 'flex-end',
-        marginRight: sizes.margins["2sm"]
-        
+        alignItems: "flex-end",
+        marginRight: sizes.margins["2sm"],
     }
 
     const value_style: any = {
@@ -69,11 +65,14 @@ export default function item ({
         color: ColorTheme().textDisabled,
     }
 
-    async function handlePress () {
-        if(secure) {
-            const isAuthenticated = await TouchID.requestBioAuth(t("Make sure it's you"), t("You're changing your password"))
-            if (isAuthenticated) navigation.navigate('SettingsNavigator', {screen: navigateTo })
-        } else navigation.navigate('SettingsNavigator', {screen: navigateTo })
+    async function handlePress() {
+        if (secure) {
+            const isAuthenticated = await TouchID.requestBioAuth(
+                t("Make sure it's you"),
+                t("You're changing your password")
+            )
+            if (isAuthenticated) navigation.navigate("SettingsNavigator", { screen: navigateTo })
+        } else navigation.navigate("SettingsNavigator", { screen: navigateTo })
     }
 
     return (
@@ -82,23 +81,24 @@ export default function item ({
                 <Text style={text_style}>{name}</Text>
             </View>
             <View style={container_right}>
-                {type == 'IMAGE' ?
-                    <View style={[value_container, {marginRight: 6}]}>
+                {type == "IMAGE" ? (
+                    <View style={[value_container, { marginRight: 6 }]}>
                         <UserShow.Root data={session.user}>
-                            <UserShow.ProfilePicture displayOnMoment={false} pictureDimensions={{width: 22, height: 22}} disableAnalytics={true}/>
-                        </UserShow.Root>                    
+                            <UserShow.ProfilePicture
+                                displayOnMoment={false}
+                                pictureDimensions={{ width: 22, height: 22 }}
+                                disableAnalytics={true}
+                            />
+                        </UserShow.Root>
                     </View>
-
-                    :
+                ) : (
                     <View style={value_container}>
-                      <Text style={value_style}>{value}</Text>  
+                        <Text style={value_style}>{value}</Text>
                     </View>
-                    
-                }
+                )}
                 <View>
-                    <ChevronRight fill={icon_fill} width={16} height={16}/>
+                    <ChevronRight fill={icon_fill} width={16} height={16} />
                 </View>
-                
             </View>
         </Pressable>
     )
