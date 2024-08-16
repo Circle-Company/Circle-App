dad9432aa6a76cec98b3750d25c30c9dc5476148
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-08-16 14:07:10 -0300

    add: haptic-feedback hook + declaration

--- src/lib/hooks/useHapticFeedback.ts
+++ src/lib/hooks/useHapticFeedback.ts
@@ -1,10 +1,35 @@
 import ReactNativeHapticFeedback from "react-native-haptic-feedback"
 
+export type HapticFeedbackTypes =
+    | "impactLight"
+    | "impactMedium"
+    | "impactHeavy"
+    | "rigid"
+    | "soft"
+    | "notificationSuccess"
+    | "notificationWarning"
+    | "notificationError"
+    | "selection"
+    | "clockTick"
+    | "clockCLick"
+    | "contextClick"
+    | "keyboardPress"
+    | "keyboardRelease"
+    | "keyboardTap"
+    | "longPress"
+    | "textHandleMove"
+    | "virtualKey"
+    | "virtualKeyRelease"
+    | "effectClick"
+    | "effectDoubleClick"
+    | "effectHeavyClick"
+    | "effectTick"
+
 const options = {
-    enableVibrateFallback: true,
-    ignoreAndroidSystemSettings: false,
+    enableVibrateFallback: false,
+    ignoreAndroidSystemSettings: true,
 }
 
-export function Vibrate() {
-    ReactNativeHapticFeedback.trigger("impactMedium", options)
+export function Vibrate(hapticType: HapticFeedbackTypes) {
+    ReactNativeHapticFeedback.trigger(hapticType, options)
 }
