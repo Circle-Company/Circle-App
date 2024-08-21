1ab28cf5c74a1bbaa50172db2858822c5498fe6f
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-08-21 00:40:38 -0300

    add: get userPreferences when useEffect

--- src/contexts/Preferences/index.tsx
+++ src/contexts/Preferences/index.tsx
@@ -1,10 +1,15 @@
 import React from "react"
+import PersistedContext from "../Persisted"
 import { Provider as LanguageProvider } from "./language"
 
-export function Provider({children}: React.PropsWithChildren<{}>){
-    return (
-        <LanguageProvider>
-            {children}
-        </LanguageProvider>
-    )
+export function Provider({ children }: React.PropsWithChildren<{}>) {
+    const { session } = React.useContext(PersistedContext)
+
+    React.useEffect(() => {
+        async function getPreferences() {
+            await session.preferences.get(session.user.id)
+        }
+        getPreferences()
+    }, [])
+    return <LanguageProvider>{children}</LanguageProvider>
 }
