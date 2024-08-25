f37e74d8b6c673e2705c7a77ba4e0a4a2684473c
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-08-25 11:43:11 -0300

    add(account/header): add notification badge

--- src/components/headers/account/account-header_left.tsx
+++ src/components/headers/account/account-header_left.tsx
@@ -1,3 +1,5 @@
+import { BadgeIcon } from "@/components/general/badge-icon"
+import PersistedContext from "@/contexts/Persisted"
 import { useNavigation } from "@react-navigation/native"
 import React from "react"
 import { View } from "react-native"
@@ -7,12 +9,14 @@ import sizes from "../../../layout/constants/sizes"
 import ButtonStandart from "../../buttons/button-standart"
 
 export default function AccountHeaderLeft() {
+    const { session } = React.useContext(PersistedContext)
     const navigation = useNavigation()
 
     const container: any = {
         flexDirection: "row",
         marginLeft: sizes.margins["3sm"],
     }
+
     function handlePress() {
         navigation.navigate("InboxNavigator", { screen: "Inbox" })
     }
@@ -26,6 +30,7 @@ export default function AccountHeaderLeft() {
             >
                 <Icon fill={String(ColorTheme().text)} width={18} height={18} />
             </ButtonStandart>
+            <BadgeIcon number={session.account.unreadNotificationsCount} />
         </View>
     )
 }
