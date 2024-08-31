cf75b70c40b1ea34e2c6ee570c97246840510a1b
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-08-30 22:56:53 -0300

    add: metro config

--- metro.config.js
+++ metro.config.js
@@ -6,11 +6,29 @@ module.exports = (async () => {
     } = await getDefaultConfig()
     return {
         transformer: {
+            getTransformOptions: async () => ({
+                transform: {
+                    experimentalImportSupport: false,
+                    inlineRequires: true,
+                },
+            }),
             babelTransformerPath: require.resolve("react-native-svg-transformer"),
         },
         resolver: {
             assetExts: assetExts.filter((ext) => ext !== "svg"),
             sourceExts: [...sourceExts, "svg"],
+        },
+        server: {
+            enhanceMiddleware: (middleware) => {
+                return (req, res, next) => {
+                    if (req.url === "/debugger-ui") {
+                        res.statusCode = 200
+                        res.end("") // Responde com vazio para evitar o erro
+                        return
+                    }
+                    return middleware(req, res, next)
+                }
+            },
         },
     }
 })()
