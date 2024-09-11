db526bf46a3b27e338bafb8dbcc8ae5ef3a86354
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-09-10 22:50:24 -0300

    add: complete skeleton structure

--- src/features/list-notifications/skeleton.tsx
+++ src/features/list-notifications/skeleton.tsx
@@ -16,7 +16,6 @@ export function ListNotificationsSkeleton() {
 
     const itemContainer = {
         marginTop: sizes.margins["1md"],
-        height: sizes.sizes["2lg"],
         flexDirection: "row",
         justifyContent: "center",
     }
@@ -25,28 +24,273 @@ export function ListNotificationsSkeleton() {
     }
 
     const itemContainerRight = {
-        flex: 1,
         alignItems: "flex-start",
         justifyContent: "center",
     }
     return (
         <View style={container}>
             <View style={sectionContainer}>
-                <Skeleton.View style={{ width: 70, height: 15, borderRadius: 7.5 }} />
+                <Skeleton.View
+                    duration={3000}
+                    style={{ width: 70, height: 15, borderRadius: 7.5 }}
+                />
                 <View style={itemContainer}>
                     <View style={itemContainerLeft}>
-                        <Skeleton.View style={{ width: 55, height: 55, borderRadius: 50 }} />
+                        <Skeleton.View
+                            duration={3000}
+                            style={{ width: 55, height: 55, borderRadius: 50 }}
+                        />
                     </View>
                     <View style={itemContainerRight}>
                         <Skeleton.View
+                            duration={3000}
                             style={{
                                 width: 55,
                                 height: 14,
                                 borderRadius: 50,
-                                marginBottom: sizes.margins["1sm"],
+                                marginBottom: sizes.margins["2sm"],
                             }}
                         />
-                        <Skeleton.View style={{ width: 130, height: 14, borderRadius: 50 }} />
+                        <Skeleton.View
+                            duration={3000}
+                            style={{ width: 130, height: 14, borderRadius: 50 }}
+                        />
+                    </View>
+                </View>
+                <View style={itemContainer}>
+                    <View style={itemContainerLeft}>
+                        <Skeleton.View
+                            duration={3000}
+                            style={{ width: 55, height: 55, borderRadius: 50 }}
+                        />
+                    </View>
+                    <View style={itemContainerRight}>
+                        <Skeleton.View
+                            duration={3000}
+                            style={{
+                                width: 55,
+                                height: 14,
+                                borderRadius: 50,
+                                marginBottom: sizes.margins["2sm"],
+                            }}
+                        />
+                        <Skeleton.View
+                            duration={3000}
+                            style={{ width: 130, height: 14, borderRadius: 50 }}
+                        />
+                    </View>
+                </View>
+                <View style={itemContainer}>
+                    <View style={itemContainerLeft}>
+                        <Skeleton.View
+                            duration={3000}
+                            style={{ width: 55, height: 55, borderRadius: 50 }}
+                        />
+                    </View>
+                    <View style={itemContainerRight}>
+                        <Skeleton.View
+                            duration={3000}
+                            style={{
+                                width: 55,
+                                height: 14,
+                                borderRadius: 50,
+                                marginBottom: sizes.margins["2sm"],
+                            }}
+                        />
+                        <Skeleton.View
+                            duration={3000}
+                            style={{ width: 130, height: 14, borderRadius: 50 }}
+                        />
+                    </View>
+                </View>
+                <View style={itemContainer}>
+                    <View style={itemContainerLeft}>
+                        <Skeleton.View
+                            duration={3000}
+                            style={{ width: 55, height: 55, borderRadius: 50 }}
+                        />
+                    </View>
+                    <View style={itemContainerRight}>
+                        <Skeleton.View
+                            duration={3000}
+                            style={{
+                                width: 55,
+                                height: 14,
+                                borderRadius: 50,
+                                marginBottom: sizes.margins["2sm"],
+                            }}
+                        />
+                        <Skeleton.View
+                            duration={3000}
+                            style={{ width: 130, height: 14, borderRadius: 50 }}
+                        />
+                    </View>
+                </View>
+                <View style={itemContainer}>
+                    <View style={itemContainerLeft}>
+                        <Skeleton.View
+                            duration={3000}
+                            style={{ width: 55, height: 55, borderRadius: 50 }}
+                        />
+                    </View>
+                    <View style={itemContainerRight}>
+                        <Skeleton.View
+                            duration={3000}
+                            style={{
+                                width: 55,
+                                height: 14,
+                                borderRadius: 50,
+                                marginBottom: sizes.margins["2sm"],
+                            }}
+                        />
+                        <Skeleton.View
+                            duration={3000}
+                            style={{ width: 130, height: 14, borderRadius: 50 }}
+                        />
+                    </View>
+                </View>
+                <View style={itemContainer}>
+                    <View style={itemContainerLeft}>
+                        <Skeleton.View
+                            duration={3000}
+                            style={{ width: 55, height: 55, borderRadius: 50 }}
+                        />
+                    </View>
+                    <View style={itemContainerRight}>
+                        <Skeleton.View
+                            duration={3000}
+                            style={{
+                                width: 55,
+                                height: 14,
+                                borderRadius: 50,
+                                marginBottom: sizes.margins["2sm"],
+                            }}
+                        />
+                        <Skeleton.View
+                            duration={3000}
+                            style={{ width: 130, height: 14, borderRadius: 50 }}
+                        />
+                    </View>
+                </View>
+                <View style={itemContainer}>
+                    <View style={itemContainerLeft}>
+                        <Skeleton.View
+                            duration={3000}
+                            style={{ width: 55, height: 55, borderRadius: 50 }}
+                        />
+                    </View>
+                    <View style={itemContainerRight}>
+                        <Skeleton.View
+                            duration={3000}
+                            style={{
+                                width: 55,
+                                height: 14,
+                                borderRadius: 50,
+                                marginBottom: sizes.margins["2sm"],
+                            }}
+                        />
+                        <Skeleton.View
+                            duration={3000}
+                            style={{ width: 130, height: 14, borderRadius: 50 }}
+                        />
+                    </View>
+                </View>
+            </View>
+            <View style={sectionContainer}>
+                <Skeleton.View
+                    duration={3000}
+                    style={{ width: 70, height: 15, borderRadius: 7.5 }}
+                />
+                <View style={itemContainer}>
+                    <View style={itemContainerLeft}>
+                        <Skeleton.View
+                            duration={3000}
+                            style={{ width: 55, height: 55, borderRadius: 50 }}
+                        />
+                    </View>
+                    <View style={itemContainerRight}>
+                        <Skeleton.View
+                            duration={3000}
+                            style={{
+                                width: 55,
+                                height: 14,
+                                borderRadius: 50,
+                                marginBottom: sizes.margins["2sm"],
+                            }}
+                        />
+                        <Skeleton.View
+                            duration={3000}
+                            style={{ width: 130, height: 14, borderRadius: 50 }}
+                        />
+                    </View>
+                </View>
+                <View style={itemContainer}>
+                    <View style={itemContainerLeft}>
+                        <Skeleton.View
+                            duration={3000}
+                            style={{ width: 55, height: 55, borderRadius: 50 }}
+                        />
+                    </View>
+                    <View style={itemContainerRight}>
+                        <Skeleton.View
+                            duration={3000}
+                            style={{
+                                width: 55,
+                                height: 14,
+                                borderRadius: 50,
+                                marginBottom: sizes.margins["2sm"],
+                            }}
+                        />
+                        <Skeleton.View
+                            duration={3000}
+                            style={{ width: 130, height: 14, borderRadius: 50 }}
+                        />
+                    </View>
+                </View>
+                <View style={itemContainer}>
+                    <View style={itemContainerLeft}>
+                        <Skeleton.View
+                            duration={3000}
+                            style={{ width: 55, height: 55, borderRadius: 50 }}
+                        />
+                    </View>
+                    <View style={itemContainerRight}>
+                        <Skeleton.View
+                            duration={3000}
+                            style={{
+                                width: 55,
+                                height: 14,
+                                borderRadius: 50,
+                                marginBottom: sizes.margins["2sm"],
+                            }}
+                        />
+                        <Skeleton.View
+                            duration={3000}
+                            style={{ width: 130, height: 14, borderRadius: 50 }}
+                        />
+                    </View>
+                </View>
+                <View style={itemContainer}>
+                    <View style={itemContainerLeft}>
+                        <Skeleton.View
+                            duration={3000}
+                            style={{ width: 55, height: 55, borderRadius: 50 }}
+                        />
+                    </View>
+                    <View style={itemContainerRight}>
+                        <Skeleton.View
+                            duration={3000}
+                            style={{
+                                width: 55,
+                                height: 14,
+                                borderRadius: 50,
+                                marginBottom: sizes.margins["2sm"],
+                            }}
+                        />
+                        <Skeleton.View
+                            duration={3000}
+                            style={{ width: 130, height: 14, borderRadius: 50 }}
+                        />
                     </View>
                 </View>
             </View>
