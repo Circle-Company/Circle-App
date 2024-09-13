401855ab43ab72b9d21ca8351c837bf321dc0f8f
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-09-13 20:12:37 -0300

    add: fix routes + vibrate

--- src/components/comment/components/comments-render_comment.tsx
+++ src/components/comment/components/comments-render_comment.tsx
@@ -1,5 +1,6 @@
+import Button from "@/components/buttons/button-standart"
 import React from "react"
-import { Pressable, View } from "react-native"
+import { View } from "react-native"
 import HeartIconOutline from "../../../assets/icons/svgs/heart_2-outline.svg"
 import HeartIcon from "../../../assets/icons/svgs/heart_2.svg"
 import PersistedContext from "../../../contexts/Persisted"
@@ -7,9 +8,10 @@ import { timeDifferenceConverter } from "../../../helpers/dateConversor"
 import ColorTheme from "../../../layout/constants/colors"
 import fonts from "../../../layout/constants/fonts"
 import sizes from "../../../layout/constants/sizes"
+import { Vibrate } from "../../../lib/hooks/useHapticFeedback"
 import api from "../../../services/Api"
-import MomentContext from "../../moment/context"
 import { Text } from "../../Themed"
+import MomentContext from "../../moment/context"
 import { UserShow } from "../../user_show"
 import { CommentsRenderCommentProps } from "../comments-types"
 
@@ -17,7 +19,7 @@ export default function render_comment({ comment, preview }: CommentsRenderComme
     const { session } = React.useContext(PersistedContext)
     const { momentUserActions } = React.useContext(MomentContext)
 
-    const [like, setLike] = React.useState(false)
+    const [like, setLike] = React.useState(comment.is_liked)
 
     const container: any = {
         flexDirection: "row",
@@ -39,9 +41,9 @@ export default function render_comment({ comment, preview }: CommentsRenderComme
     }
     const container_right: any = {
         alignItems: "center",
-        justifyContent: "flex-start",
-        paddingLeft: sizes.margins["3sm"],
-        paddingTop: sizes.margins["2sm"] * 0.7,
+        justifyContent: "center",
+        marginLeft: sizes.margins["3sm"],
+        marginTop: sizes.margins["2sm"] * 0.7,
     }
     const like_container: any = {
         width: "100%",
@@ -74,20 +76,24 @@ export default function render_comment({ comment, preview }: CommentsRenderComme
         if (like) {
             momentUserActions.setLikeComment(false)
             setLike(false)
+            Vibrate("effectTick")
             await api
-                .post("/moment/unlike-comment", {
-                    user_id: session.user.id,
-                    comment_id: comment.id,
-                })
+                .post(
+                    `/moments/comments/${comment.id}/unlike`,
+                    {},
+                    { headers: { authorization_token: session.account.jwtToken } }
+                )
                 .catch((error) => console.log(error))
         } else {
             momentUserActions.setLikeComment(true)
             setLike(true)
+            Vibrate("effectClick")
             await api
-                .post("/moment/like-comment", {
-                    user_id: session.user.id,
-                    comment_id: comment.id,
-                })
+                .post(
+                    `/moments/comments/${comment.id}/like`,
+                    {},
+                    { headers: { authorization_token: session.account.jwtToken } }
+                )
                 .catch((error) => console.log(error))
         }
     }
@@ -125,19 +131,31 @@ export default function render_comment({ comment, preview }: CommentsRenderComme
                 </View>
                 <Text style={content_style}>{comment.content}</Text>
             </View>
-            <Pressable onPress={handleLikePress} style={container_right}>
+            <Button
+                action={handleLikePress}
+                height={sizes.sizes["2md"] * 0.7}
+                width={sizes.sizes["2md"] * 0.7}
+                style={container_right}
+                backgroundColor={ColorTheme().backgroundDisabled + 90}
+            >
                 <View style={like_container}>
                     {like ? (
-                        <HeartIcon fill={ColorTheme().like.toString()} width={15} height={15} />
+                        <HeartIcon
+                            fill={ColorTheme().like.toString()}
+                            style={{ top: 1 }}
+                            width={12}
+                            height={12}
+                        />
                     ) : (
                         <HeartIconOutline
                             fill={`${ColorTheme().textDisabled}60`}
-                            width={15}
-                            height={15}
+                            style={{ top: 1 }}
+                            width={13}
+                            height={13}
                         />
                     )}
                 </View>
-            </Pressable>
+            </Button>
         </View>
     )
 }
