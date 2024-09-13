ca7cfca08e5f66202efd91c9a4f07aa981c405bf
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-09-13 20:26:36 -0300

    add: vibration + eslint refactor

--- src/components/buttons/button-standart.tsx
+++ src/components/buttons/button-standart.tsx
@@ -1,43 +1,45 @@
-import React from 'react'
-import { Pressable, View, Text, Animated} from 'react-native'
-import ColorTheme from '../../layout/constants/colors'
-import {useNavigation} from '@react-navigation/native'
-import Camera from '../../../assets/icons/svgs/camera.svg'
-import sizes from '../../layout/constants/sizes'
-import fonts from '../../layout/constants/fonts'
+import React from "react"
+import { Animated, Pressable } from "react-native"
+import ColorTheme from "../../layout/constants/colors"
+import sizes from "../../layout/constants/sizes"
 
 type ButtonStandartProps = {
+    style?: any
     bounciness?: number
     animationScale?: number
-    width?: number | string,
-    height?: number,
-    backgroundColor?: string,
-    children: React.ReactNode,
+    width?: number | string
+    height?: number
+    backgroundColor?: string
+    children: React.ReactNode
     margins?: boolean
     borderRadius?: number
-    action(): void,
+    action(): void
+    vibrate?: () => void
 }
 
 export default function button_standart({
+    style,
     bounciness = 12,
     animationScale = 0.8,
-    width = sizes.buttons.height*0.5,
-    height = sizes.buttons.height*0.5,
+    width = sizes.buttons.height * 0.5,
+    height = sizes.buttons.height * 0.5,
     backgroundColor = String(ColorTheme().backgroundDisabled),
     children,
     margins = true,
-    borderRadius = Number(width)/2,
+    borderRadius = Number(width) / 2,
     action,
+    vibrate,
 }: ButtonStandartProps) {
-
     var animatedScale = React.useRef(new Animated.Value(1)).current
-    React.useEffect(() => { animatedScale.setValue(1) }, [])
+    React.useEffect(() => {
+        animatedScale.setValue(1)
+    }, [])
     const HandleButtonAnimation = () => {
         Animated.spring(animatedScale, {
             toValue: 1,
             bounciness: bounciness,
             speed: 10,
-            useNativeDriver: true
+            useNativeDriver: true,
         }).start()
     }
 
@@ -46,30 +48,37 @@ export default function button_standart({
             toValue: animationScale,
             bounciness: bounciness,
             speed: 20,
-            useNativeDriver: true
+            useNativeDriver: true,
         }).start()
     }
 
     const container: any = {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
         backgroundColor: backgroundColor,
-        marginRight: margins? sizes.margins['3sm']: 0,
+        marginRight: margins ? sizes.margins["3sm"] : 0,
         width: width,
         height: height,
-        borderRadius: borderRadius
+        borderRadius: borderRadius,
+        ...style,
     }
-
 
     async function onPress() {
         HandleButtonAnimation
+        if (vibrate) vibrate()
         action()
     }
-    return(
-        <Animated.View style={{transform: [{ scale: animatedScale }] }}>
-            <Pressable style={container} onPressIn={HandlePressIn} onPressOut={HandleButtonAnimation} onPress={onPress}>{children}</Pressable>
-        </Animated.View>          
-
+    return (
+        <Animated.View style={{ transform: [{ scale: animatedScale }] }}>
+            <Pressable
+                style={container}
+                onPressIn={HandlePressIn}
+                onPressOut={HandleButtonAnimation}
+                onPress={onPress}
+            >
+                {children}
+            </Pressable>
+        </Animated.View>
     )
 }
