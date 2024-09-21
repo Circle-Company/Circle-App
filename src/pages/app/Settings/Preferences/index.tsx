ee35453529579178ad41e344468b7fdda7e8c587
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-09-21 12:52:04 -0300

    add: organize (language + notification + haptic feedback) to preferences screen

--- src/pages/app/Settings/Preferences/index.tsx
+++ src/pages/app/Settings/Preferences/index.tsx
@@ -1,43 +1,41 @@
+import LanguageContext from "@/contexts/Preferences/language"
+import ColorTheme from "@/layout/constants/colors"
 import React from "react"
 import { FlatList, ScrollView } from "react-native"
 import { Settings } from "../../../../components/settings"
 
+import NotificationIcon from "../../../../assets/icons/svgs/bell-outline.svg"
+import HapticsIcon from "../../../../assets/icons/svgs/iphone_radiowaves_left_and_right.svg"
+import LanguageIcon from "../../../../assets/icons/svgs/textformat_alt.svg"
+
 export default function PreferencesScreen() {
+    const { t } = React.useContext(LanguageContext)
     const ListData = [
         {
-            name: "Language",
+            name: t("App"),
             content: [
                 {
-                    name: "App Language",
-                    value: null,
-                    type: "TEXT",
-                    navigateTo: "Settings-Privacy-Policy",
-                    secure: false,
-                },
-            ],
-        },
-        {
-            name: "Content",
-            content: [
-                {
-                    name: "Auto Play",
+                    icon: <LanguageIcon width={20} height={20} fill={ColorTheme().text} />,
+                    name: t("Language"),
                     value: null,
                     type: "TEXT",
                     navigateTo: "Settings-Privacy-Policy",
                     secure: false,
                 },
                 {
-                    name: "Haptic Feedback",
+                    icon: <HapticsIcon width={20} height={20} fill={ColorTheme().text} />,
+                    name: t("Haptic Feedback"),
                     value: null,
                     type: "TEXT",
                     navigateTo: "Settings-Privacy-Policy",
                     secure: false,
                 },
                 {
-                    name: "Content Translation",
+                    icon: <NotificationIcon width={20} height={20} fill={ColorTheme().text} />,
+                    name: t("Notifications"),
                     value: null,
                     type: "TEXT",
-                    navigateTo: "Settings-Privacy-Policy",
+                    navigateTo: "Settings-Preferences-PushNotifications",
                     secure: false,
                 },
             ],
