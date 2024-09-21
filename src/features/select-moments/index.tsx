bd81b26b7e4a0359a568a88ec0c9845c3f7e0f3e
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-09-21 15:01:54 -0300

    add: linear gradient + space

--- src/features/select-moments/index.tsx
+++ src/features/select-moments/index.tsx
@@ -1,9 +1,10 @@
 import React from "react"
-import { FlatList, ScrollView, View } from "react-native"
+import { FlatList, ScrollView, View, useColorScheme } from "react-native"
+import LinearGradient from "react-native-linear-gradient"
 import { Text } from "../../components/Themed"
 import LanguageContext from "../../contexts/Preferences/language"
 import SelectMomentsContext from "../../contexts/selectMoments"
-import ColorTheme from "../../layout/constants/colors"
+import ColorTheme, { colors } from "../../layout/constants/colors"
 import fonts from "../../layout/constants/fonts"
 import sizes from "../../layout/constants/sizes"
 import RenderMoment from "./components/render-moments"
@@ -32,22 +33,43 @@ export default function ListSelectMoments() {
     React.useEffect(() => {
         get_moments()
     }, [])
+
+    const isDarkMode = useColorScheme() === "dark"
+    const gradient = isDarkMode
+        ? [colors.gray.black + "00", colors.gray.black]
+        : [colors.gray.white + "00", colors.gray.white]
     return (
-        <ScrollView showsVerticalScrollIndicator={false}>
-            <View style={header_container}>
-                <Text style={header_text}>{t("Select Moments to put in the Memory")}</Text>
-            </View>
-            <FlatList
-                numColumns={3}
-                data={all_moments}
-                scrollEnabled={false}
-                keyExtractor={(item: any) => item.id}
-                contentContainerStyle={item_container}
-                columnWrapperStyle={{ justifyContent: "space-between" }}
-                renderItem={({ item }) => {
-                    return <RenderMoment moment={item} />
+        <>
+            <ScrollView style={{ flex: 1 }} showsVerticalScrollIndicator={false}>
+                <View style={header_container}>
+                    <Text style={header_text}>{t("Select Moments to put in the Memory")}</Text>
+                </View>
+                <FlatList
+                    numColumns={3}
+                    data={all_moments}
+                    scrollEnabled={false}
+                    keyExtractor={(item: any) => item.id}
+                    contentContainerStyle={item_container}
+                    columnWrapperStyle={{ justifyContent: "space-between" }}
+                    renderItem={({ item }) => {
+                        return <RenderMoment moment={item} />
+                    }}
+                    ListFooterComponent={() => {
+                        return <View style={{ height: 120 }} />
+                    }}
+                />
+            </ScrollView>
+            <LinearGradient
+                style={{
+                    zIndex: 1,
+                    position: "absolute",
+                    bottom: 0,
+                    left: 0,
+                    width: sizes.screens.width,
+                    height: 200,
                 }}
+                colors={gradient}
             />
-        </ScrollView>
+        </>
     )
 }
