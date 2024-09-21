4b8153a2d5050377647ceb107043befb6932c5d1
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-09-21 14:31:24 -0300

    add: desactive icons

--- src/pages/app/Settings/Preferences/index.tsx
+++ src/pages/app/Settings/Preferences/index.tsx
@@ -1,12 +1,7 @@
 import LanguageContext from "@/contexts/Preferences/language"
-import ColorTheme from "@/layout/constants/colors"
 import React from "react"
 import { FlatList, ScrollView } from "react-native"
 import { Settings } from "../../../../components/settings"
-
-import NotificationIcon from "../../../../assets/icons/svgs/bell-outline.svg"
-import HapticsIcon from "../../../../assets/icons/svgs/iphone_radiowaves_left_and_right.svg"
-import LanguageIcon from "../../../../assets/icons/svgs/textformat_alt.svg"
 
 export default function PreferencesScreen() {
     const { t } = React.useContext(LanguageContext)
@@ -15,7 +10,7 @@ export default function PreferencesScreen() {
             name: t("App"),
             content: [
                 {
-                    icon: <LanguageIcon width={20} height={20} fill={ColorTheme().text} />,
+                    //icon: <LanguageIcon width={20} height={20} fill={ColorTheme().text} />,
                     name: t("Language"),
                     value: null,
                     type: "TEXT",
@@ -23,7 +18,7 @@ export default function PreferencesScreen() {
                     secure: false,
                 },
                 {
-                    icon: <HapticsIcon width={20} height={20} fill={ColorTheme().text} />,
+                    //icon: <HapticsIcon width={20} height={20} fill={ColorTheme().text} />,
                     name: t("Haptic Feedback"),
                     value: null,
                     type: "TEXT",
@@ -31,7 +26,7 @@ export default function PreferencesScreen() {
                     secure: false,
                 },
                 {
-                    icon: <NotificationIcon width={20} height={20} fill={ColorTheme().text} />,
+                    //icon: <NotificationIcon width={20} height={20} fill={ColorTheme().text} />,
                     name: t("Notifications"),
                     value: null,
                     type: "TEXT",
