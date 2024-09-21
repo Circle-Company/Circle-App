1c252544fe2c913b192abc0fd1a7af6c50a5aa7f
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-09-21 12:39:48 -0300

    remove: flipper settings

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,5 @@
 import { MMKV } from "react-native-mmkv"
-import { initializeMMKVFlipper } from "react-native-mmkv-flipper-plugin"
 export const storage = new MMKV()
-
-if (__DEV__) {
-    initializeMMKVFlipper({ default: storage })
-}
 
 export function storageKeys() {
     const sessionId = storage.getNumber("@circle:sessionId")
