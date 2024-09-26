4c94cd5e1f2e43a6f95f9eb093c0289c86151df7
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-09-26 01:38:24 -0300

    add: moment preview

--- src/components/notification/components/render-notification.tsx
+++ src/components/notification/components/render-notification.tsx
@@ -1,34 +1,38 @@
-import React from 'react'
-import sizes from '../../../layout/constants/sizes'
-import { NotificationProps } from '../notification-types'
-import { Notification } from '..'
-import { UserShow } from '../../user_show'
-import { Text } from '../../Themed'
+import React from "react"
+import { Notification } from ".."
+import { UserShow } from "../../user_show"
+import { NotificationProps } from "../notification-types"
 
 type renderNotificationProps = {
-    notification: NotificationProps,
+    notification: NotificationProps
 }
 
-export default function render_notification ({notification}: renderNotificationProps) {
+export default function render_notification({ notification }: renderNotificationProps) {
     return (
         <Notification.Container.Main notification={notification}>
             <Notification.Container.Left>
                 <UserShow.Root data={notification.sender_user}>
-                    <UserShow.ProfilePicture pictureDimensions={{width: 50, height: 50}} displayOnMoment={false}/>
+                    <UserShow.ProfilePicture
+                        pictureDimensions={{ width: 50, height: 50 }}
+                        displayOnMoment={false}
+                    />
                 </UserShow.Root>
-                <Notification.Seal/>    
+                <Notification.Seal />
             </Notification.Container.Left>
 
             <Notification.Container.Center>
-            <Notification.Text/>
+                <Notification.Text />
             </Notification.Container.Center>
 
             <Notification.Container.Right>
-            <UserShow.Root data={notification.sender_user}>
-                    <UserShow.FollowButton isFollowing={notification.you_follow}/>
-                </UserShow.Root>
+                {notification.midia?.nhd_resolution ? (
+                    <Notification.Preview />
+                ) : (
+                    <UserShow.Root data={notification.sender_user}>
+                        <UserShow.FollowButton isFollowing={notification.you_follow} />
+                    </UserShow.Root>
+                )}
             </Notification.Container.Right>
-
         </Notification.Container.Main>
     )
 }
