1b429d7853dad445d7fa25a9d16b48f1f5430001
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-09-26 01:34:40 -0300

    refactor: eslint

--- src/features/list-notifications/end-reached.tsx
+++ src/features/list-notifications/end-reached.tsx
@@ -1,20 +1,20 @@
-import { Text, View } from "../../../../components/Themed";
-import ColorTheme from "../../../../layout/constants/colors";
-import fonts from "../../../../layout/constants/fonts";
-import sizes from "../../../../layout/constants/sizes";
+import { Text, View } from "@/components/Themed"
+import ColorTheme from "@/layout/constants/colors"
+import fonts from "@/layout/constants/fonts"
+import sizes from "@/layout/constants/sizes"
 
-export default function endReached ({text}: {text: string}) {
+export default function endReached({ text }: { text: string }) {
     const container: any = {
         width: sizes.screens.width,
         height: sizes.headers.height,
-        alignItems: 'center',
-        justifyContent: 'center',
+        alignItems: "center",
+        justifyContent: "center",
     }
 
     const title: any = {
-        fontSize: fonts.size.body * 0.9,
+        fontSize: fonts.size.body * 0.8,
         fontFamily: fonts.family.Semibold,
-        color: ColorTheme().textDisabled
+        color: ColorTheme().textDisabled,
     }
 
     return (
