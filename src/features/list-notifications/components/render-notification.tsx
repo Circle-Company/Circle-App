aae43593ce36442c112b18abbe06ffb015002c1c
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-09-26 01:40:25 -0300

    refactor: eslint

--- src/features/list-notifications/components/render-notification.tsx
+++ src/features/list-notifications/components/render-notification.tsx
@@ -1,28 +1,30 @@
-import React from 'react'
-import sizes from '../../../layout/constants/sizes'
-import { NotificationProps } from '../../../components/notification/notification-types'
-import { Notification } from '../../../components/notification'
-import { UserShow } from '../../../components/user_show'
-import { Text } from '../../../components/Themed'
-import { View } from 'react-native'
-import { MidiaRender } from '../../../components/midia_render'
+import React from "react"
+import { View } from "react-native"
+import { MidiaRender } from "../../../components/midia_render"
+import { Notification } from "../../../components/notification"
+import { NotificationProps } from "../../../components/notification/notification-types"
+import { UserShow } from "../../../components/user_show"
+import sizes from "../../../layout/constants/sizes"
 
 type renderNotificationProps = {
-    notification: NotificationProps,
+    notification: NotificationProps
 }
 
-export default function render_notification ({notification}: renderNotificationProps) {
+export default function render_notification({ notification }: renderNotificationProps) {
     return (
-        <View style={{paddingVertical: sizes.paddings['2sm']}}>
+        <View style={{ paddingVertical: sizes.paddings["2sm"] }}>
             <Notification.Container.Main notification={notification}>
                 <Notification.Container.Left>
                     <UserShow.Root data={notification.sender_user}>
-                        <UserShow.ProfilePicture pictureDimensions={{width: 50, height: 50}} displayOnMoment={false}/>
+                        <UserShow.ProfilePicture
+                            pictureDimensions={{ width: 50, height: 50 }}
+                            displayOnMoment={false}
+                        />
                     </UserShow.Root>
-                    <Notification.Seal/>
+                    <Notification.Seal />
                 </Notification.Container.Left>
                 <Notification.Container.Center>
-                    <Notification.Text/>
+                    <Notification.Text />
                 </Notification.Container.Center>
                 <Notification.Container.Right>
                     {notification.midia ? (
@@ -38,8 +40,7 @@ export default function render_notification ({notification}: renderNotificationP
                         </UserShow.Root>
                     )}
                 </Notification.Container.Right>
-            </Notification.Container.Main>            
+            </Notification.Container.Main>
         </View>
-
     )
 }
