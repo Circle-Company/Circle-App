72b0100330f30677b1c71567a9dbd9656b344e0a
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-09-28 13:50:33 -0300

    refactor: eslint

--- src/features/list-search/index.tsx
+++ src/features/list-search/index.tsx
@@ -1,52 +1,38 @@
-import { FlatList } from "react-native"
-import { Text, View } from "../../components/Themed"
 import React from "react"
-import search_list from "../../data/search_list.json"
+import { FlatList } from "react-native"
 import { Search } from "../../components/search"
-import { UserShow } from "../../components/user_show"
 import { useSearchContext } from "../../components/search/search-context"
-import NearToYouUsers from "../list-near_to_you"
-import NearToYouContext, { NearToYouContextProvider } from "../../components/near_to_you/near_to_you-context"
-interface SearchResult {
-
-}
+interface SearchResult {}
 
 export default function ListSearch() {
-
-    const { searchTerm, fetchData, isConnected} = useSearchContext()
+    const { searchTerm, fetchData, isConnected } = useSearchContext()
     const [searchResults, setSearchResults] = React.useState<SearchResult[]>([])
 
     // Use useEffect para atualizar a lista quando o searchTerm mudar
     React.useEffect(() => {
         const fetchDataFromApi = async () => {
-        try {
-            const data = await fetchData(searchTerm)
-            setSearchResults(data)
-            
-        } catch (error) {
-            console.error(error)
+            try {
+                const data = await fetchData(searchTerm)
+                setSearchResults(data)
+            } catch (error) {
+                console.error(error)
+            }
         }
-    }
 
-        fetchDataFromApi();
+        fetchDataFromApi()
     }, [searchTerm, fetchData])
 
-    
-    if(isConnected == false) return <Search.Offline/>
+    if (isConnected == false) return <Search.Offline />
     else {
         return (
-            <View>
-                <FlatList
+            <FlatList
                 scrollEnabled={false}
                 data={searchResults}
                 keyExtractor={(item: any) => item.id}
-                renderItem={({item}) => {
-                    return (
-                        <Search.RenderUser user={item}/>
-                    )
+                renderItem={({ item }) => {
+                    return <Search.RenderUser user={item} />
                 }}
-                />
-            </View>
-        )           
+            />
+        )
     }
 }
