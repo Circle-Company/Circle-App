9eadf584b93fa7ba509d9a55331580aa99b2cf4c
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-10-21 20:29:32 -0300

    add: cancel button

---
+++ src/components/buttons/cancel.tsx
@@ -0,0 +1,84 @@
+import ColorTheme from "@/layout/constants/colors"
+import fonts from "@/layout/constants/fonts"
+import React from "react"
+import { Animated, Pressable } from "react-native"
+import sizes from "../../layout/constants/sizes"
+import { Text } from "../Themed"
+
+type ButtonStandartProps = {
+    style?: any
+    text: string
+    textColor?: string
+    bounciness?: number
+    animationScale?: number
+    width?: number | string
+    height?: number
+    action(): void
+    vibrate?: () => void
+}
+
+export default function CancelButton({
+    style,
+    text,
+    textColor = ColorTheme().error,
+    bounciness = 12,
+    animationScale = 0.8,
+    width = "100%",
+    height = sizes.buttons.height * 0.5,
+    action,
+    vibrate,
+}: ButtonStandartProps) {
+    var animatedScale = React.useRef(new Animated.Value(1)).current
+    React.useEffect(() => {
+        animatedScale.setValue(1)
+    }, [])
+    const HandleButtonAnimation = () => {
+        Animated.spring(animatedScale, {
+            toValue: 1,
+            bounciness: bounciness,
+            speed: 10,
+            useNativeDriver: true,
+        }).start()
+    }
+
+    const HandlePressIn = () => {
+        Animated.spring(animatedScale, {
+            toValue: animationScale,
+            bounciness: bounciness,
+            speed: 20,
+            useNativeDriver: true,
+        }).start()
+    }
+
+    const container: any = {
+        alignItems: "center",
+        justifyContent: "center",
+        width: width,
+        height: height,
+        ...style,
+    }
+
+    const textStyle: any = {
+        fontSize: fonts.size.body,
+        fontFamily: fonts.family.Medium,
+        color: textColor,
+    }
+
+    async function onPress() {
+        HandleButtonAnimation
+        if (vibrate) vibrate()
+        action()
+    }
+    return (
+        <Animated.View style={{ transform: [{ scale: animatedScale }] }}>
+            <Pressable
+                style={container}
+                onPressIn={HandlePressIn}
+                onPressOut={HandleButtonAnimation}
+                onPress={onPress}
+            >
+                <Text style={textStyle}>{text}</Text>
+            </Pressable>
+        </Animated.View>
+    )
+}
