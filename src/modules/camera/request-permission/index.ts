63e5f9968182052be6f53498284d2f8d6c52f3bb
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-12-28 10:52:31 -0300

    fix: camera module compatibility

--- src/modules/camera/request-permission/index.ts
+++
@@ -1,8 +0,0 @@
-import { useCameraPermission } from "react-native-vision-camera"
-
-const { hasPermission, requestPermission } = useCameraPermission()
-
-export function requestCameraModulePermissions() {
-    if (hasPermission) return
-    else requestPermission()
-}
