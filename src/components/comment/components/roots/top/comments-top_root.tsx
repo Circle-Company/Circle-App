082ddfc511a7fd62425185fbad0ae699b34129e3
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-12-28 10:45:29 -0300

    refactor(comments): eslint

--- src/components/comment/components/roots/top/comments-top_root.tsx
+++ src/components/comment/components/roots/top/comments-top_root.tsx
@@ -1,16 +1,22 @@
+import { useCommentsContext } from "@/components/comment/comments-context"
 import React from "react"
 import { View } from "react-native"
-import { CommentsTopRootProps } from "../../../comments-types"
 import sizes from "../../../../../layout/constants/sizes"
+import { CommentsTopRootProps } from "../../../comments-types"
 
-export default function top_root ({children}: CommentsTopRootProps) {
+export default function top_root({ children }: CommentsTopRootProps) {
+    const { comment, preview } = useCommentsContext()
+
     const container: any = {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingTop: sizes.paddings["1sm"]*1.3,
-        paddingLeft: sizes.paddings["1md"]*0.7,
-        paddingRight: sizes.paddings["1md"]*1.4,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        paddingTop: sizes.paddings["1sm"] * 1.3,
+        paddingLeft: sizes.paddings["1md"] * 0.7,
+        paddingRight: sizes.paddings["1md"] * 1.4,
     }
-    return <View style={container}>{children}</View>
+
+    return (
+        <View style={container}>{preview && comment?.comments?.length == 0 ? null : children}</View>
+    )
 }
