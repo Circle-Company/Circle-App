f4ffdd1c88c17351148e791094ac9b548b32a08e
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-12-28 10:49:06 -0300

    add(media-render): increase gradient + eslint refactor

--- src/components/midia_render/components/midia_render-root.tsx
+++ src/components/midia_render/components/midia_render-root.tsx
@@ -1,11 +1,10 @@
 import React from "react"
 import { View } from "react-native"
-import { MidiaRenderMainRootProps } from "../midia_render-types"
-import MidiaRenderContext from "../midia_render-context"
 import LinearGradient from "react-native-linear-gradient"
+import MidiaRenderContext from "../midia_render-context"
+import { MidiaRenderMainRootProps } from "../midia_render-types"
 
-export default function root ({children, data, content_sizes}: MidiaRenderMainRootProps) {
-
+export default function root({ children, data, content_sizes }: MidiaRenderMainRootProps) {
     const container: any = {
         width: content_sizes.width,
         height: content_sizes.height,
@@ -13,33 +12,24 @@ export default function root ({children, data, content_sizes}: MidiaRenderMainRo
     }
     const superior_gradient: any = {
         width: content_sizes.width,
-        height: content_sizes.height/6,
-        position:'absolute',
+        height: content_sizes.height / 6,
+        position: "absolute",
         zIndex: 1,
-        top: 0
+        top: 0,
     }
 
     const inferior_gradient: any = {
         width: content_sizes.width,
-        height: content_sizes.height/3,
-        position:'absolute',
+        height: content_sizes.height / 3,
+        position: "absolute",
         zIndex: 1,
-        bottom: 0
+        bottom: 0,
     }
     return (
-        <MidiaRenderContext.Provider value={{midia: data, content_sizes: content_sizes }}>
-            <LinearGradient
-                colors={['#00000030', '#00000000']}
-                style={superior_gradient}
-            />
-            <LinearGradient
-                colors={['#00000000', '#00000040']}
-                style={inferior_gradient}
-            />
-            <View style={container}>
-                {children}
-            </View>            
+        <MidiaRenderContext.Provider value={{ midia: data, content_sizes: content_sizes }}>
+            <LinearGradient colors={["#00000060", "#00000000"]} style={superior_gradient} />
+            <LinearGradient colors={["#00000000", "#00000040"]} style={inferior_gradient} />
+            <View style={container}>{children}</View>
         </MidiaRenderContext.Provider>
-
     )
 }
