0106dea4fdf6edc69fcb659d5b33be5ba5936471
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-12-28 10:50:58 -0300

    add(list-moments): preview option on context + eslint refactor

--- src/features/list-moments/components/render-comment-full.tsx
+++ src/features/list-moments/components/render-comment-full.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { Comments } from '../../../components/comment'
-import MomentContext from '../../../components/moment/context'
+import React from "react"
+import { Comments } from "../../../components/comment"
+import MomentContext from "../../../components/moment/context"
 
-export default function render_comment_full () {
+export default function render_comment_full() {
     const { momentData, momentOptions } = React.useContext(MomentContext)
     return (
-        <Comments.MainRoot data={momentData.comments}>
+        <Comments.MainRoot data={momentData.comments} preview={false}>
             <Comments.Container focused={momentOptions.isFocused}>
                 <Comments.CenterRoot>
-                    <Comments.ListComments preview={false}/>                    
+                    <Comments.ListComments />
                 </Comments.CenterRoot>
             </Comments.Container>
-        </Comments.MainRoot>      
+        </Comments.MainRoot>
     )
 }
