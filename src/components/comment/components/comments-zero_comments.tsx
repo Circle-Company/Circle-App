198633f8a03b7618129e540894f27c6994cc300e
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-12-29 16:59:07 -0300

    add(comments): zeroComments handlePress

--- src/components/comment/components/comments-zero_comments.tsx
+++ src/components/comment/components/comments-zero_comments.tsx
@@ -1,6 +1,7 @@
 import PlusIcon from "@/assets/icons/svgs/plus_circle.svg"
 import { Text } from "@/components/Themed"
 import ButtonStandart from "@/components/buttons/button-standart"
+import FeedContext from "@/contexts/Feed"
 import LanguageContext from "@/contexts/Preferences/language"
 import ColorTheme, { colors } from "@/layout/constants/colors"
 import fonts from "@/layout/constants/fonts"
@@ -8,9 +9,10 @@ import sizes from "@/layout/constants/sizes"
 import { Vibrate } from "@/lib/hooks/useHapticFeedback"
 import React from "react"
 import { View, useColorScheme } from "react-native"
-
 export default function ZeroComments() {
     const { t } = React.useContext(LanguageContext)
+    const { commentEnabled, setCommentEnabled, setKeyboardVisible, setScrollEnabled } =
+        React.useContext(FeedContext)
     const isDarkMode = useColorScheme() === "dark"
     const container: any = {
         width: sizes.screens.width * 0.7,
@@ -40,42 +42,62 @@ export default function ZeroComments() {
         fontSize: fonts.size.body * 0.9,
         marginRight: sizes.margins["1sm"],
     }
+    function handlePress() {
+        if (commentEnabled) {
+            setCommentEnabled(false)
+            setScrollEnabled(true)
+            setKeyboardVisible(false)
+        } else {
+            setCommentEnabled(true)
+            setKeyboardVisible(true)
+            setScrollEnabled(true)
+        }
+    }
 
-    return (
-        <View style={container}>
-            <View
-                style={{
-                    marginBottom: sizes.margins["2sm"],
-                    alignItems: "center",
-                    alignSelf: "center",
-                }}
-            >
-                <Text style={{ fontSize: fonts.size.caption1, color: ColorTheme().textDisabled }}>
-                    {t("This Moment has no comments.")}
-                </Text>
-                <Text
+    if (commentEnabled) return null
+    else
+        return (
+            <View style={container}>
+                <View
                     style={{
-                        fontFamily: fonts.family.Semibold,
-
-                        fontSize: fonts.size.subheadline * 0.85,
+                        marginBottom: sizes.margins["2sm"],
+                        alignItems: "center",
+                        alignSelf: "center",
                     }}
                 >
-                    {t("Be the first to comment.")}
-                </Text>
-            </View>
+                    <Text
+                        style={{ fontSize: fonts.size.caption1, color: ColorTheme().textDisabled }}
+                    >
+                        {t("This Moment has no comments.")}
+                    </Text>
+                    <Text
+                        style={{
+                            fontFamily: fonts.family.Semibold,
 
-            <ButtonStandart
-                action={() => {}}
-                vibrate={() => {
-                    Vibrate("effectTick")
-                }}
-                margins={false}
-                bounciness={2}
-                style={buttonContainer}
-            >
-                <Text style={buttonTitle}>{t("Add Comment")}</Text>
-                <PlusIcon style={{ top: -0.5 }} width={16} height={16} fill={ColorTheme().text} />
-            </ButtonStandart>
-        </View>
-    )
+                            fontSize: fonts.size.subheadline * 0.85,
+                        }}
+                    >
+                        {t("Be the first to comment.")}
+                    </Text>
+                </View>
+
+                <ButtonStandart
+                    action={handlePress}
+                    vibrate={() => {
+                        Vibrate("effectTick")
+                    }}
+                    margins={false}
+                    bounciness={2}
+                    style={buttonContainer}
+                >
+                    <Text style={buttonTitle}>{t("Add Comment")}</Text>
+                    <PlusIcon
+                        style={{ top: -0.5 }}
+                        width={16}
+                        height={16}
+                        fill={ColorTheme().text}
+                    />
+                </ButtonStandart>
+            </View>
+        )
 }
