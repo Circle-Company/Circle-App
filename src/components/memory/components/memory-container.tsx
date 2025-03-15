bf5bcd3350ccb22abff05b3c9b7855fb7b4a9ae3
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-03-15 14:27:42 -0300

    refactor: eslint

--- src/components/memory/components/memory-container.tsx
+++ src/components/memory/components/memory-container.tsx
@@ -1,39 +1,39 @@
 import React from "react"
 import { View } from "react-native"
-import { MemoryContainerProps } from "../memory-types"
+import ColorTheme from "../../../layout/constants/colors"
 import sizes from "../../../layout/constants/sizes"
 import { MidiaRender } from "../../midia_render"
-import ColorTheme from "../../../layout/constants/colors"
+import { MemoryContainerProps } from "../memory-types"
 
-export default function container ({
+export default function container({
     children,
     contentRender,
     focused,
-    contentSizes = sizes.moment.tiny
+    contentSizes = sizes.moment.tiny,
 }: MemoryContainerProps) {
-
-    const container:any = {
+    const container: any = {
         ...sizes.moment.tiny,
-        overflow: 'hidden',
+        overflow: "hidden",
         borderRadius: sizes.moment.tiny.borderRadius,
         backgroundColor: ColorTheme().backgroundDisabled,
-        zIndex: 1
+        zIndex: 1,
     }
-    const content_container:any = {
-        position: 'absolute',
+    const content_container: any = {
+        position: "absolute",
         width: sizes.moment.tiny,
         height: sizes.moment.tiny,
-        zIndex: 0
+        zIndex: 0,
     }
 
     return (
         <View style={container}>
             <View style={content_container}>
                 <MidiaRender.Root data={contentRender} content_sizes={contentSizes}>
-                <MidiaRender.RenderImage blur={focused} blurRadius={12}/>  
-                </MidiaRender.Root>                
+                    <MidiaRender.RenderImage blur={focused} blurRadius={18} />
+                </MidiaRender.Root>
             </View>
+
             {children}
         </View>
-    )           
+    )
 }
--- src/components/midia_render/components/midia_render-image.tsx
+++ src/components/midia_render/components/midia_render-image.tsx
@@ -1,85 +1,94 @@
-import React, { useState, useEffect } from "react";
-import { View, Image, Animated } from "react-native";
-import FastImage from "react-native-fast-image";
-import ColorTheme, { colors } from "../../../layout/constants/colors";
-import { useMidiaRenderContext } from "../midia_render-context";
+import React, { useState } from "react"
+import { Animated, Image } from "react-native"
+import FastImage from "react-native-fast-image"
+import ColorTheme from "../../../layout/constants/colors"
+import { useMidiaRenderContext } from "../midia_render-context"
 
 type RenderImageProps = {
-  blur?: boolean,
-  blurRadius?: number,
-  blurColor?: string
-  enableBlur?: boolean
-  isFeed?: boolean
+    blur?: boolean
+    blurRadius?: number
+    blurColor?: string
+    enableBlur?: boolean
+    isFeed?: boolean
 }
 
-export default function render_image ({
-  blur = false,
-  blurRadius = 35,
-  blurColor = String(ColorTheme().background),
-  enableBlur = true,
-  isFeed = false
+export default function render_image({
+    blur = false,
+    blurRadius = 35,
+    blurColor = String(ColorTheme().background),
+    enableBlur = true,
+    isFeed = false,
 }: RenderImageProps) {
-  const { content_sizes, midia } = useMidiaRenderContext();
-  const [fadeAnim] = useState(new Animated.Value( enableBlur? 1 : 0));
-  const image: any = {
-    ...content_sizes
-  };
-
-  const opacity_view: any = {
-    position: 'absolute',
-    zIndex: 3,
-    backgroundColor: blurColor,
-  };
-
-  function removeBlur() {
-    if(!blur){
-      Animated.timing(fadeAnim, {
-        toValue: 0,
-        duration: 600, // Tempo da transição (em milissegundos)
-        useNativeDriver: true, // Usa o driver nativo para melhor performance
-      }).start();      
-    } else {
-      Animated.timing(fadeAnim, {
-        toValue: 1,
-        duration: 600, // Tempo da transição (em milissegundos)
-        useNativeDriver: true, // Usa o driver nativo para melhor performance
-      }).start();
+    const { content_sizes, midia } = useMidiaRenderContext()
+    const [fadeAnim] = useState(new Animated.Value(enableBlur ? 1 : 0))
+    const image: any = {
+        ...content_sizes,
     }
-  }
 
-  if(isFeed) {
-    return (
-      <Image
-      source={{ uri: midia.fullhd_resolution?.toString() }}
-      style={image}
-      resizeMode="cover"
-      blurRadius={blur? blurRadius: 0}
-    />
-    )
-  }
+    const opacity_view: any = {
+        position: "absolute",
+        zIndex: 3,
+        backgroundColor: blurColor,
+    }
 
-  return (
-      <>
-          <Animated.View style={[opacity_view, {opacity: fadeAnim }]}>
+    function removeBlur() {
+        if (!blur) {
+            Animated.timing(fadeAnim, {
+                toValue: 0,
+                duration: 600, // Tempo da transição (em milissegundos)
+                useNativeDriver: true, // Usa o driver nativo para melhor performance
+            }).start()
+        } else {
+            Animated.timing(fadeAnim, {
+                toValue: 1,
+                duration: 600, // Tempo da transição (em milissegundos)
+                useNativeDriver: true, // Usa o driver nativo para melhor performance
+            }).start()
+        }
+    }
+
+    if (isFeed) {
+        return (
             <Image
-              source={{ uri: midia.nhd_resolution?.toString() }}
-              style={image}
-              resizeMode="cover"
-              blurRadius={blurRadius}
+                source={{ uri: midia?.fullhd_resolution?.toString() }}
+                style={image}
+                resizeMode="cover"
+                blurRadius={blur ? blurRadius : 0}
             />
-          </Animated.View>     
-          {!blur &&
-            <FastImage
-              source={{
-                uri: String(midia.fullhd_resolution ? midia.fullhd_resolution : midia.nhd_resolution),
-                priority: FastImage.priority.high,
-              }}
-              style={image}
-              resizeMode="cover"
-              onLoadEnd={() => {removeBlur()}}
-            />
-          }   
+        )
+    }
 
-      </>
-    );
-  };
+    if (!midia) {
+        return null
+    }
+
+    return (
+        <>
+            <Animated.View style={[opacity_view, { opacity: fadeAnim }]}>
+                <Image
+                    source={{ uri: midia?.nhd_resolution?.toString() }}
+                    style={image}
+                    resizeMode="cover"
+                    blurRadius={blurRadius}
+                />
+            </Animated.View>
+            {!blur && (
+                <FastImage
+                    source={{
+                        uri: String(
+                            midia?.fullhd_resolution
+                                ? midia?.fullhd_resolution
+                                : midia?.nhd_resolution
+                        ),
+                        priority: FastImage.priority.high,
+                    }}
+                    style={image}
+                    resizeMode="cover"
+                    onLoadEnd={() => {
+                        removeBlur()
+                    }}
+                />
+            )}
+        </>
+    )
+}
