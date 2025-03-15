4441e0780f3ef003ce94ccc47273fe1dc6bdc0c0
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-03-15 14:27:42 -0300

    refactor: eslint

--- src/components/profile/components/profile-description.tsx
+++ src/components/profile/components/profile-description.tsx
@@ -1,19 +1,14 @@
 import React from "react"
-import { View, useColorScheme, Animated} from "react-native"
-import { Text } from "../../Themed"
-import sizes from "../../../layout/constants/sizes"
+import { Animated, useColorScheme } from "react-native"
 import fonts from "../../../layout/constants/fonts"
-import { colors } from "../../../layout/constants/colors"
-import { ProfileDescriptionProps } from "../profile-types"
+import sizes from "../../../layout/constants/sizes"
+import { Text } from "../../Themed"
 import { useProfileContext } from "../profile-context"
-import ColorTheme from "../../../layout/constants/colors"
-import { opacity } from "react-native-reanimated/lib/typescript/reanimated2/Colors"
+import { ProfileDescriptionProps } from "../profile-types"
 
-export default function description ({
-}: ProfileDescriptionProps) {
-
-    const {user} = useProfileContext()
-    const isDarkMode = useColorScheme() === 'dark'
+export default function description({}: ProfileDescriptionProps) {
+    const { user } = useProfileContext()
+    const isDarkMode = useColorScheme() === "dark"
 
     var animatedScale = React.useRef(new Animated.Value(1)).current
     var animatedOpacity = React.useRef(new Animated.Value(0)).current
@@ -24,7 +19,7 @@ export default function description ({
             bounciness: 0,
             speed: 30,
             useNativeDriver: true,
-            delay: 90
+            delay: 90,
         }).start()
     }
 
@@ -32,33 +27,27 @@ export default function description ({
         handleAnimation()
     }, [])
 
-    const container:any = {
+    const container: any = {
         width: sizes.screens.width,
-        marginTop: sizes.margins["1sm"],
         paddingHorizontal: sizes.paddings["1md"],
-        paddingVertical: sizes.paddings["2sm"],
-        borderTopWidth: sizes.borders['1md'],
-        borderBottomWidth: sizes.borders['1md'],
-        borderColor: ColorTheme().backgroundDisabled,
-        backgroundColor: isDarkMode? colors.gray.grey_09: colors.gray.grey_01,
         transform: [{ scale: animatedScale }],
-        opacity: animatedOpacity
+        opacity: animatedOpacity,
     }
-    const description_style:any = {
+    const description_style: any = {
         lineHeight: 18,
         fontSize: fonts.size.body,
         fontFamily: fonts.family.Medium,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
+        flexDirection: "row",
+        textAlign: "center",
     }
 
-    if(!user.description){
+    if (!user.description) {
         return null
     }
-    
+
     return (
         <Animated.View style={container}>
-            <Text style={description_style}>{user.description}</Text>  
+            <Text style={description_style}>{user.description}</Text>
         </Animated.View>
     )
 }
