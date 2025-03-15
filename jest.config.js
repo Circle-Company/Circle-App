6e10a03dfaa352d48b6840c2839057d153455f9c
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-03-15 15:32:43 -0300

    add: setup jest

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  preset: 'react-native',
-};
+    preset: "react-native",
+    testEnvironment: "node",
+    transform: {
+        "^.+\\.(js|jsx|ts|tsx)$": "babel-jest", // Garante suporte a TSX/JSX
+    },
+    setupFilesAfterEnv: ["<rootDir>/jest.setup.js", "@testing-library/jest-native/extend-expect"],
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    moduleNameMapper: {
+        "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    },
+    collectCoverage: false,
+    collectCoverageFrom: ["src/**/*.{ts,tsx}"],
+    coverageDirectory: "coverage",
+    clearMocks: true,
+}
