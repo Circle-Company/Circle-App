e5560e50925f441d2f6d1764c274ea92d1cb4cf6
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-03-17 23:47:34 -0300

    add: header right skeleton to load

--- src/components/headers/profile/profile-header_right.tsx
+++ src/components/headers/profile/profile-header_right.tsx
@@ -1,3 +1,4 @@
+import { Skeleton } from "@/components/skeleton"
 import React from "react"
 import { View } from "react-native"
 import PersistedContext from "../../../contexts/Persisted"
@@ -14,24 +15,37 @@ export default function ProfileHeaderRight() {
         setIsLoading(true),
             setTimeout(() => {
                 setIsLoading(false)
-            }, 200)
+            }, 400)
     }, [])
     const container: any = {
         flexDirection: "row",
         alignItems: "center",
     }
     if (session.user.id == userProfile.id) return null
-    if (isLoading) return null
     else
         return (
             <View style={container}>
                 <View style={{ marginRight: sizes.margins["3sm"] }}>
                     <UserShow.Root data={userProfile}>
-                        <UserShow.FollowButton
-                            isFollowing={userProfile.you_follow}
-                            hideOnFollowing={false}
-                            displayOnMoment={false}
-                        />
+                        {isLoading ? (
+                            <Skeleton.View
+                                delay={0}
+                                duration={400}
+                                style={{
+                                    marginTop: sizes.margins["1sm"] * 0.7,
+                                    width: 70,
+                                    height: 24,
+                                    marginLeft: sizes.margins["2sm"],
+                                    borderRadius: 7,
+                                }}
+                            />
+                        ) : (
+                            <UserShow.FollowButton
+                                isFollowing={userProfile.you_follow}
+                                hideOnFollowing={false}
+                                displayOnMoment={false}
+                            />
+                        )}
                     </UserShow.Root>
                 </View>
             </View>
