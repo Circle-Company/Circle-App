bab02c5f08cc9204a60a18a1d81fad52d219e884
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-03-20 20:58:36 -0300

    add: signIn + signup routes

--- src/services/Api/routes/auth/index.ts
+++ src/services/Api/routes/auth/index.ts
@@ -1,9 +1,20 @@
 import api from "../.."
+import { SignInProps, SignUpProps, refreshTokenProps } from "./styles"
 
-async function refreshToken({ username, id }: { username: string; id: string }) {
+async function refreshToken({ username, id }: refreshTokenProps) {
     return await api.post(`/auth/refresh-token`, { username, id })
+}
+
+async function signIn({ username, password }: SignInProps): Promise<void> {
+    await api.post(`/auth/sign-in`, { username, password })
+}
+
+async function signUp({ username, password }: SignUpProps): Promise<void> {
+    await api.post(`/auth/sign-up`, { username, password })
 }
 
 export const routes = {
     refreshToken,
+    signIn,
+    signUp,
 }
