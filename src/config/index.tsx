bda411de00d768c6b11b3bc598845ace98be5509
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-03-26 20:49:24 -0300

    fix: remove watermelonDB

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,15 +1,5 @@
 //@ts-nocheck
-import {
-    API_VERSION,
-    APP_VERSION,
-    DEBUG,
-    DEV_API_ENDPOINT,
-    LOG_DEBUG,
-    LOG_LEVEL,
-    MIXPANEL_KEY,
-    NODE_ENV,
-    PROD_API_ENDPOINT,
-} from "@env"
+import { API_VERSION, APP_VERSION, DEBUG, LOG_DEBUG, LOG_LEVEL, MIXPANEL_KEY, NODE_ENV } from "@env"
 import emails from "./emails"
 import metadata from "./metadata"
 
@@ -23,7 +13,7 @@ const environment = {
 
 const api = {
     API_VERSION: API_VERSION,
-    ENDPOINT: environment.PRODUCTION ? DEV_API_ENDPOINT : PROD_API_ENDPOINT,
+    ENDPOINT: "192.168.15.10:3000",
 }
 
 const analytics = {
