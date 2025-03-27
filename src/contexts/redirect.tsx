82050e549d195498a0ef04fdf2e1ed1ef0674701
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-03-26 21:25:50 -0300

    fix: remove appData from redirect context

--- src/contexts/redirect.tsx
+++ src/contexts/redirect.tsx
@@ -2,19 +2,15 @@ import React from "react"
 
 type RedirectContextType = {
     redirectTo: "APP" | "AUTH" | "SPLASH"
-    appData: any
     setRedirectTo: (value: "APP" | "AUTH" | "SPLASH") => void
-    setAppData: React.Dispatch<React.SetStateAction<object>>
 }
 
 export const RedirectContext = React.createContext<RedirectContextType>({} as RedirectContextType)
 
 export function Provider({ children }: { children: React.ReactNode }) {
     const [redirectTo, setRedirectTo] = React.useState<"APP" | "AUTH" | "SPLASH">("SPLASH")
-    const [appData, setAppData] = React.useState({})
-
     return (
-        <RedirectContext.Provider value={{ redirectTo, appData, setRedirectTo, setAppData }}>
+        <RedirectContext.Provider value={{ redirectTo, setRedirectTo }}>
             {children}
         </RedirectContext.Provider>
     )
