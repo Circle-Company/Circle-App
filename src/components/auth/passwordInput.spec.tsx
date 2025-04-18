6c0d4726c8956219b9c905069e4e3e9f1389d9ab
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-04-18 17:01:30 -0300

    add(auth): complete inputs + screens + usecases tests ( integration)

--- src/components/auth/passwordInput.spec.tsx
+++ src/components/auth/passwordInput.spec.tsx
@@ -3,23 +3,48 @@ import React from "react"
 import AuthContext from "../../contexts/Auth"
 import PasswordInput from "./passwordInput"
 
+type MockAuthContextProps = {
+    setSignInputPassword: jest.Mock
+    signInputUsername: string
+    errorMessage: string
+    loading: boolean
+}
+
 const MockAuthProvider = ({ children }: { children: React.ReactNode }) => {
-    const authValues: any = {
+    const authValues: MockAuthContextProps = {
         setSignInputPassword: jest.fn(),
         signInputUsername: "",
         errorMessage: "",
         loading: false,
     }
-    return <AuthContext.Provider value={authValues}>{children}</AuthContext.Provider>
+    return <AuthContext.Provider value={authValues as any}>{children}</AuthContext.Provider>
 }
 
 describe("PasswordInput Component", () => {
+    beforeAll(() => {
+        jest.useFakeTimers()
+    })
+
+    afterEach(() => {
+        jest.clearAllMocks()
+        jest.clearAllTimers()
+        jest.runOnlyPendingTimers()
+    })
+
+    afterAll(() => {
+        jest.useRealTimers()
+    })
+
     it("deve renderizar corretamente os botões de limpar e visibilidade após digitar a senha", async () => {
         const { getByTestId, queryByTestId } = render(
             <MockAuthProvider>
                 <PasswordInput type="signUp" />
             </MockAuthProvider>
         )
+
+        await act(async () => {
+            jest.runAllTimers()
+        })
 
         expect(getByTestId("password-input")).toBeTruthy()
         expect(queryByTestId("password-toggle-visibility")).toBeNull()
@@ -28,6 +53,7 @@ describe("PasswordInput Component", () => {
         // Dispara o evento de digitação com act
         await act(async () => {
             fireEvent.changeText(getByTestId("password-input"), "minhaSenha123")
+            jest.runAllTimers()
         })
 
         // Espera que os botões tenham sido renderizados
--- src/pages/auth/SignIn/signIn.spec.tsx
+++ src/pages/auth/SignIn/signIn.spec.tsx
@@ -1,21 +1,41 @@
-import AuthContext from "@/contexts/Auth"
-import { fireEvent, render } from "@testing-library/react-native"
+import AuthContext, { AuthContextsData } from "@/contexts/Auth"
+import { SessionDataType } from "@/contexts/Persisted/types"
+import { NavigationContainer } from "@react-navigation/native"
+import { act, fireEvent, render, waitFor } from "@testing-library/react-native"
 import React from "react"
 import SignInScreen from "./index"
+
 const mockedColorScheme = jest.fn()
+const mockedNavigate = jest.fn()
 
 jest.mock("@/components/loading", () => ({
     Loading: {
         ActivityIndicator: () => null,
     },
 }))
+
 jest.mock("react-native/Libraries/Utilities/useColorScheme", () => ({
     ...jest.requireActual("react-native/Libraries/Utilities/useColorScheme"),
-    useColorScheme: mockedColorScheme,
+    default: mockedColorScheme,
 }))
 
-const mockAuthContext = (overrides = {}) => ({
+jest.mock("@react-navigation/native", () => {
+    const actualNav = jest.requireActual("@react-navigation/native")
+    return {
+        ...actualNav,
+        useNavigation: () => ({
+            navigate: mockedNavigate,
+            goBack: jest.fn(),
+        }),
+    }
+})
+
+const mockAuthContext = (overrides = {}): AuthContextsData => ({
     signIn: jest.fn(),
+    signUp: jest.fn(),
+    signOut: jest.fn(),
+    checkIsSigned: jest.fn(),
+    getSessionData: jest.fn(),
     setSignInputUsername: jest.fn(),
     setSignInputPassword: jest.fn(),
     setErrorMessage: jest.fn(),
@@ -23,46 +43,70 @@ const mockAuthContext = (overrides = {}) => ({
     signInputPassword: "",
     loading: false,
     errorMessage: "",
+    sessionData: {} as SessionDataType,
     ...overrides,
 })
 
 const renderWithProvider = (contextOverrides = {}) => {
-    const contextValue: any = mockAuthContext(contextOverrides)
+    const contextValue = mockAuthContext(contextOverrides)
 
     const utils = render(
-        <AuthContext.Provider value={contextValue}>
-            <SignInScreen />
-        </AuthContext.Provider>
+        <NavigationContainer>
+            <AuthContext.Provider value={contextValue}>
+                <SignInScreen />
+            </AuthContext.Provider>
+        </NavigationContainer>
     )
 
     return { ...utils, contextValue }
 }
 
 describe("SignInScreen", () => {
-    mockedColorScheme.mockImplementationOnce(() => "dark")
-    it("renderiza os principais elementos da tela", () => {
-        const { getByTestId } = renderWithProvider()
-
-        expect(getByTestId("title")).toBeTruthy()
-        expect(getByTestId("inputs-container")).toBeTruthy()
-        expect(getByTestId("handle-submit")).toBeTruthy()
-        expect(getByTestId("auth-terms")).toBeTruthy()
+    beforeAll(() => {
+        jest.useFakeTimers()
     })
 
-    it("não chama signIn se os campos estiverem vazios", () => {
-        const { getByTestId, contextValue } = renderWithProvider({
-            signInputUsername: "",
-            signInputPassword: "",
-            loading: false,
-            errorMessage: "",
+    beforeEach(() => {
+        mockedColorScheme.mockImplementation(() => "dark")
+    })
+
+    afterEach(() => {
+        jest.clearAllMocks()
+        jest.clearAllTimers()
+        jest.runOnlyPendingTimers()
+    })
+
+    afterAll(() => {
+        jest.useRealTimers()
+    })
+
+    it("renderiza os principais elementos da tela", async () => {
+        const { getByTestId } = renderWithProvider()
+
+        await act(async () => {
+            jest.runAllTimers()
         })
 
-        fireEvent.press(getByTestId("handle-submit"))
+        await waitFor(() => {
+            expect(getByTestId("title")).toBeTruthy()
+            expect(getByTestId("inputs-container")).toBeTruthy()
+            expect(getByTestId("handle-submit")).toBeTruthy()
+            expect(getByTestId("auth-terms")).toBeTruthy()
+        })
+    })
+
+    it("não chama signIn se os campos estiverem vazios", async () => {
+        const { getByTestId, contextValue } = renderWithProvider()
+
+        await act(async () => {
+            fireEvent.press(getByTestId("handle-submit"))
+            jest.runAllTimers()
+        })
 
         expect(contextValue.signIn).not.toHaveBeenCalled()
     })
 
-    it("chama signIn se os campos estiverem preenchidos e não estiver carregando", () => {
+    it("chama signIn se os campos estiverem preenchidos e não estiver carregando", async () => {
         const { getByTestId, contextValue } = renderWithProvider({
             signInputUsername: "username",
             signInputPassword: "password",
@@ -70,11 +114,15 @@ describe("SignInScreen", () => {
             errorMessage: "",
         })
 
-        fireEvent.press(getByTestId("handle-submit"))
+        await act(async () => {
+            fireEvent.press(getByTestId("handle-submit"))
+            jest.runAllTimers()
+        })
+
         expect(contextValue.signIn).toHaveBeenCalled()
     })
 
-    it("não chama signIn se os campos estiverem preenchidos e carregando", () => {
+    it("não chama signIn se os campos estiverem preenchidos e carregando", async () => {
         const { getByTestId, contextValue } = renderWithProvider({
             signInputUsername: "username",
             signInputPassword: "password",
@@ -82,7 +130,11 @@ describe("SignInScreen", () => {
             errorMessage: "",
         })
 
-        fireEvent.press(getByTestId("handle-submit"))
+        await act(async () => {
+            fireEvent.press(getByTestId("handle-submit"))
+            jest.runAllTimers()
+        })
+
         expect(contextValue.signIn).not.toHaveBeenCalled()
     })
 
@@ -92,6 +144,10 @@ describe("SignInScreen", () => {
             signInputUsername: "username",
             signInputPassword: "password",
             loading: false,
+        })
+
+        await act(async () => {
+            jest.runAllTimers()
         })
 
         const errorMessage = await findByTestId("error-container")
