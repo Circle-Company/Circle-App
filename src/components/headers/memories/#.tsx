7417e2c897b6c5b7340324c80dc93bed9204b544
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-04-18 17:04:11 -0300

    fix: types + add button square prop

--- src/components/headers/memories/#.tsx
+++ src/components/headers/memories/#.tsx
@@ -10,7 +10,7 @@ export default function MemoriesHeaderLeft() {
 
     return (
         <View style={{ flexDirection: "row" }}>
-            <HeaderButton action={() => navigation.goBack()} marginLeft>
+            <HeaderButton action={() => navigation.goBack()} marginLeft square>
                 <ArrowLeft fill={String(ColorTheme().text)} width={18} height={18} />
             </HeaderButton>
         </View>
