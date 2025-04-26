4471ca9b0128b1efba9f4c0f78c977e3494fb2b3
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-04-26 15:36:19 -0300

    fix: resolve merge conflicts and update RenderStatistics component with React and ViewStyle imports

--- src/features/render-profile/render-statistics.tsx
+++ src/features/render-profile/render-statistics.tsx
@@ -1,27 +1,20 @@
-import { View } from "../../components/Themed";
-import { Profile } from "../../components/profile";
-import { ProfileReciveDataProps } from "../../components/profile/profile-types";
-import ColorTheme from "../../layout/constants/colors";
-import sizes from "../../layout/constants/sizes";
-
-type RenderStatisticsProps = {
-}
-
-export default function RenderStatistics({
-} : RenderStatisticsProps) {
-
-    const container: any = {
+import React from "react"
+import { ViewStyle } from "react-native"
+import { View } from "../../components/Themed"
+import { Profile } from "../../components/profile"
+import sizes from "../../layout/constants/sizes"
+export default function RenderStatistics() {
+    const container: ViewStyle = {
         paddingVertical: sizes.paddings["1sm"],
     }
 
-    return(
+    return (
         <View style={container}>
             <Profile.Statistics.Container>
-                <Profile.Statistics.Followers/>
-                <Profile.Statistics.Likes/>
-                <Profile.Statistics.Views/>
-            </Profile.Statistics.Container>            
+                <Profile.Statistics.Followers />
+                <Profile.Statistics.Likes />
+                <Profile.Statistics.Views />
+            </Profile.Statistics.Container>
         </View>
-
     )
 }
