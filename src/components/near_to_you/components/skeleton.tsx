b8a50c0b2c75ef4ca9f311ac54052fe62774e124
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-05-16 00:40:21 -0300

    feat: adicionar componente Skeleton para exibição de carregamento no contexto de usuários próximos

---
+++ src/components/near_to_you/components/skeleton.tsx
@@ -0,0 +1,11 @@
+import { SkeletonView } from "@/components/skeleton/skeleton-view"
+import { Text } from "@/components/Themed"
+import React from "react"
+
+export default function Skeleton() {
+    return (
+        <SkeletonView>
+            <Text>SKELETON</Text>
+        </SkeletonView>
+    )
+}
