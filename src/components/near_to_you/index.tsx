2a4767efb99f005f2df901443c4e4b04b10f4115
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-05-19 22:13:59 -0300

    refactor: remover importação não utilizada do componente NearToYou

--- src/components/near_to_you/index.tsx
+++ src/components/near_to_you/index.tsx
@@ -1,6 +1,4 @@
 import render_user from "./components/render_user"
-import Skeleton from "./components/skeleton"
 export const NearToYou = {
     RenderUser: render_user,
-    Skeleton: Skeleton,
 }
