b552826052f31a7c25df25e8fc6e7b5fc934ad8a
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-05-19 22:14:38 -0300

    refactor: reorganizar importações e adicionar o contexto Near no componente App

--- App.tsx
+++ App.tsx
@@ -1,30 +1,33 @@
-import { DefaultTheme, NavigationContainer } from "@react-navigation/native"
 import * as React from "react"
-import { GestureHandlerRootView } from "react-native-gesture-handler"
-import { KeyboardProvider } from "react-native-keyboard-controller"
+
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native"
 import { SafeAreaProvider, initialWindowMetrics } from "react-native-safe-area-context"
-import { Provider as AuthProvider } from "./src/contexts/Auth"
-import { Provider as FeedProvider } from "./src/contexts/Feed"
-import { Provider as PersistedProvider } from "./src/contexts/Persisted"
-import { Provider as PreferencesProvider } from "./src/contexts/Preferences"
-import { Provider as ToastProvider } from "./src/contexts/Toast"
+
 import { Provider as AccountProvider } from "./src/contexts/account"
+import { Provider as AuthProvider } from "./src/contexts/Auth"
 import { Provider as BottomSheetProvider } from "./src/contexts/bottomSheet"
 import { Provider as BottomTabsProvider } from "./src/contexts/bottomTabs"
+import ColorTheme from "./src/layout/constants/colors"
+import { Provider as FeedProvider } from "./src/contexts/Feed"
 import { Provider as GeolocationProvider } from "./src/contexts/geolocation"
+import { GestureHandlerRootView } from "react-native-gesture-handler"
+import { KeyboardProvider } from "react-native-keyboard-controller"
 import { Provider as MemoryProvider } from "./src/contexts/memory"
+import { Provider as NearProvider } from "./src/contexts/near"
 import { Provider as NetworkProvider } from "./src/contexts/network"
 import { Provider as NewMomentProvider } from "./src/contexts/newMoment"
 import { Provider as NotificationProvider } from "./src/contexts/notification"
+import { Provider as PersistedProvider } from "./src/contexts/Persisted"
+import { Provider as PreferencesProvider } from "./src/contexts/Preferences"
 import { Provider as ProfileProvider } from "./src/contexts/profile"
+import { QueryProvider } from "./src/lib/react-query"
 import { Provider as RedirectProvider } from "./src/contexts/redirect"
+import Routes from "./src/routes"
 import { Provider as SelectMomentsProvider } from "./src/contexts/selectMoments"
+import { Provider as ToastProvider } from "./src/contexts/Toast"
 import { Provider as TrackingProvider } from "./src/contexts/tracking"
 import { Provider as ViewProfileProvider } from "./src/contexts/viewProfile"
-import ColorTheme from "./src/layout/constants/colors"
 import sizes from "./src/layout/constants/sizes"
-import { QueryProvider } from "./src/lib/react-query"
-import Routes from "./src/routes"
 
 function InnerApp() {
     const myTheme = {
@@ -43,17 +46,20 @@ function InnerApp() {
                         <ProfileProvider>
                             <ViewProfileProvider>
                                 <FeedProvider>
-                                    <NavigationContainer theme={myTheme}>
-                                        <BottomSheetProvider>
-                                            <SelectMomentsProvider>
-                                                <MemoryProvider>
-                                                    <NewMomentProvider>
-                                                        <Routes />
-                                                    </NewMomentProvider>
-                                                </MemoryProvider>
-                                            </SelectMomentsProvider>
-                                        </BottomSheetProvider>
-                                    </NavigationContainer>
+                                    <NearProvider>
+                                        <NavigationContainer theme={myTheme}>
+                                            <BottomSheetProvider>
+                                                <SelectMomentsProvider>
+                                                    <MemoryProvider>
+                                                        <NewMomentProvider>
+                                                            <Routes />
+                                                        </NewMomentProvider>
+                                                    </MemoryProvider>
+                                                </SelectMomentsProvider>
+                                            </BottomSheetProvider>
+                                        </NavigationContainer>                                        
+                                    </NearProvider>
+
                                 </FeedProvider>
                             </ViewProfileProvider>
                         </ProfileProvider>
