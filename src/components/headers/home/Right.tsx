dcdeeb24daefcc7c08ef2ba3a6278e10e06500e2
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-05-24 22:43:33 -0300

    refactor: reorganizar importações e atualizar a lógica de navegação no componente HeaderRightHome

--- src/components/headers/home/Right.tsx
+++ src/components/headers/home/Right.tsx
@@ -1,20 +1,26 @@
-import { useNavigation } from "@react-navigation/native"
-import React from "react"
 import { Text, View } from "react-native"
-import Camera from "../../../assets/icons/svgs/plus_circle.svg"
-import LanguageContext from "../../../contexts/Preferences/language"
+
+import { BadgeIcon } from "@/components/general/badge-icon"
+import Bell from "../../../assets/icons/svgs/tray.svg"
+import ButtonStandart from "../../buttons/button-standart"
+import Camera from "../../../assets/icons/svgs/camera.svg"
 import ColorTheme from "../../../layout/constants/colors"
+import LanguageContext from "../../../contexts/Preferences/language"
+import PersistedContext from "@/contexts/Persisted"
+import React from "react"
 import fonts from "../../../layout/constants/fonts"
 import sizes from "../../../layout/constants/sizes"
-import ButtonStandart from "../../buttons/button-standart"
+import { useNavigation } from "@react-navigation/native"
 
 export default function HeaderRightHome() {
     const { t } = React.useContext(LanguageContext)
+    const {session} = React.useContext(PersistedContext)
 
     const navigation: any = useNavigation()
 
     const container: any = {
         flexDirection: "row",
+        marginRight: sizes.margins["3sm"],
     }
 
     const text: any = {
@@ -23,6 +29,12 @@ export default function HeaderRightHome() {
         color: ColorTheme().text,
     }
     const textContainer = {
+        marginRight: sizes.margins["1sm"],
+    }
+    const buttonContainer = {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
         marginRight: sizes.margins["2sm"],
     }
 
@@ -30,22 +42,35 @@ export default function HeaderRightHome() {
         navigation.navigate("CreateBottomTab")
     }
 
-    async function onPressSearch() {
-        navigation.navigate("ExploreNavigator", { screen: "ExploreScreen" })
+    async function onPressNotifications() {
+        navigation.navigate("InboxNavigator", { screen: "Inbox" })
     }
 
     return (
         <View style={container}>
+            <View style={buttonContainer}>
+                <ButtonStandart
+                    action={onPressNewMoment}
+                    margins={false}
+                    backgroundColor={String(ColorTheme().backgroundDisabled)}
+                >
+                    <View style={textContainer}>
+                        <Text style={text}>{t("Create")}</Text>
+                    </View>
+                    <Camera fill={String(ColorTheme().text)} width={16} height={16} />
+                </ButtonStandart>
+            </View>
             <ButtonStandart
-                action={onPressNewMoment}
-                width={90}
+                style={{paddingHorizontal: sizes.paddings["1sm"]}}
+                square={false}
+                action={onPressNotifications}
+                margins={false}
                 backgroundColor={String(ColorTheme().backgroundDisabled)}
             >
-                <View style={textContainer}>
-                    <Text style={text}>{t("Create")}</Text>
-                </View>
-                <Camera fill={String(ColorTheme().text)} width={16} height={16} />
+                <BadgeIcon style={{position: "relative", marginRight: 6, borderWidth: 0}}number={1} /> 
+                <Bell fill={String(ColorTheme().text)} width={16} height={16} />
             </ButtonStandart>
+            
         </View>
     )
 }
