f25f3b2edd3eb7696f1dd949f5a94980c58ee341
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-05-24 22:43:46 -0300

    refactor: reorganizar importações e otimizar estilos no componente MomentFullViews

--- src/components/moment/components/moment-full-views.tsx
+++ src/components/moment/components/moment-full-views.tsx
@@ -1,39 +1,55 @@
+import { StyleSheet, Text, View } from "react-native"
+
 import React from "react"
-import { Text, View } from "react-native"
 import Icon from "../../../assets/icons/svgs/eye.svg"
 import { formatNumberWithDots } from "../../../helpers/numberConversor"
 import ColorTheme from "../../../layout/constants/colors"
 import fonts from "../../../layout/constants/fonts"
 import sizes from "../../../layout/constants/sizes"
 
-type MomentFullViewsProps = {
+interface MomentFullViewsProps {
     color?: string
     views: number
 }
 
-export default function views({ views, color = String(ColorTheme().text) }: MomentFullViewsProps) {
-    const container: any = {
-        borderRadius: (sizes.sizes["2md"] * 0.9) / 2,
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "row",
-    }
-    const number_style: any = {
-        fontSize: fonts.size.body * 0.8,
-        fontFamily: fonts.family.Semibold,
-        color,
-    }
-    if (views == 0) return null
-    else
-        return (
-            <View style={container}>
-                <Icon
-                    fill={color}
-                    width={14}
-                    height={14}
-                    style={{ marginRight: sizes.margins["1sm"] * 1.4 }}
-                />
-                <Text style={number_style}>{formatNumberWithDots(views)}</Text>
-            </View>
-        )
+const MomentFullViews: React.FC<MomentFullViewsProps> = ({ 
+    views, 
+    color = ColorTheme().text 
+}) => {
+    if (views === 0) return null
+
+    const styles = StyleSheet.create({
+        container: {
+            alignItems: "center",
+            borderRadius: (sizes.sizes["2md"] * 0.9) / 2,
+            flexDirection: "row",
+            justifyContent: "center",
+        },
+        icon: {
+            marginRight: sizes.margins["1sm"] * 1.4,
+        },
+        numberText: {
+            color,
+            fontFamily: fonts.family.Semibold,
+            fontSize: fonts.size.body * 0.8,
+        },
+    })
+
+    if (views === 0) return null
+
+    return (
+        <View style={styles.container}>
+            <Icon
+                fill={color}
+                width={14}
+                height={14}
+                style={styles.icon}
+            />
+            <Text style={styles.numberText}>
+                {formatNumberWithDots(views)}
+            </Text>
+        </View>
+    )
 }
+
+export default MomentFullViews
