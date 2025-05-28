5e926f4a583b346856be0a7e12ca45553e3efc27
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-05-28 10:36:38 -0300

    refactor: remover padding do componente CommentsCenterRoot para simplificar a estrutura e melhorar a legibilidade do código

--- src/components/comment/components/roots/center/comments-center_root.tsx
+++ src/components/comment/components/roots/center/comments-center_root.tsx
@@ -1,13 +1,7 @@
+import { CommentsCenterRootProps } from "../../../comments-types"
 import React from "react"
 import { View } from "react-native"
-import { CommentsCenterRootProps } from "../../../comments-types"
-import sizes from "../../../../../layout/constants/sizes"
 
 export default function center_root({ children }: CommentsCenterRootProps) {
-    const container: any = {
-        paddingLeft: sizes.paddings["1md"] * 0.7,
-        paddingRight: sizes.paddings["1md"] * 1.4,
-    }
-
-    return <View style={container}>{children}</View>
+    return <View>{children}</View>
 }
