ecdd8bd0e3dd784ba60e7ca2774f2109a5f8a034
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-05-28 10:36:25 -0300

    refactor: corrigir verificação de comentários e ajustar padding no componente TopRoot para melhorar a legibilidade e a estrutura do código

--- src/components/comment/components/roots/top/comments-top_root.tsx
+++ src/components/comment/components/roots/top/comments-top_root.tsx
@@ -1,22 +1,22 @@
-import { useCommentsContext } from "@/components/comment/comments-context"
+import { CommentsTopRootProps } from "../../../comments-types"
 import React from "react"
 import { View } from "react-native"
 import sizes from "../../../../../layout/constants/sizes"
-import { CommentsTopRootProps } from "../../../comments-types"
+import { useCommentsContext } from "@/components/comment/comments-context"
 
-export default function top_root({ children }: CommentsTopRootProps) {
+export default function TopRoot({ children }: CommentsTopRootProps) {
     const { comment, preview } = useCommentsContext()
 
     const container: any = {
         flexDirection: "row",
         alignItems: "center",
         justifyContent: "center",
-        paddingTop: sizes.paddings["1sm"] * 1.3,
+        paddingTop: sizes.paddings["1sm"] * 0.8,
         paddingLeft: sizes.paddings["1md"] * 0.7,
         paddingRight: sizes.paddings["1md"] * 1.4,
     }
 
     return (
-        <View style={container}>{preview && comment?.comments?.length == 0 ? null : children}</View>
+        <View style={container}>{preview && comment?.length == 0 ? null : children}</View>
     )
 }
