67a0f05572d0511231b2ad942819b586a79d9331
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-05-28 10:37:21 -0300

    refactor: reorganizar importações no componente CommentsContainer para melhorar a legibilidade do código

--- src/components/comment/components/comments-container.tsx
+++ src/components/comment/components/comments-container.tsx
@@ -1,7 +1,7 @@
-import React from "react"
-import { View } from "react-native"
 import { CommentsContainerProps } from "../comments-types"
 import MomentContext from "../../moment/context"
+import React from "react"
+import { View } from "react-native"
 
 export default function container({ children, focused }: CommentsContainerProps) {
     const { momentSize } = React.useContext(MomentContext)
