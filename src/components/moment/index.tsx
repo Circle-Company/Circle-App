476061d58c4b78f5e4ad83854902f2cf63890621
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-05-28 10:33:57 -0300

    refactor: reorganizar importações no componente Moment, adicionando o controle de áudio e ajustando a estrutura dos componentes

--- src/components/moment/index.tsx
+++ src/components/moment/index.tsx
@@ -1,19 +1,20 @@
-import top_root from "./components/roots/top/moment-top_root"
+import audio_control from "./components/moment-audio_control"
+import bottom_root from "./components/roots/bottom/moment-bottom_root"
+import center_root from "./components/roots/center/moment-center_root"
+import comments from "./components/moment-full-comments"
+import container from "./components/moment-container"
+import date from "./components/moment-date"
+import description from "./components/moment-description"
+import like from "./components/moment-like"
+import main_root from "./components/roots/moment-main_root"
+import more from "./components/moment-more"
+import options from "./components/moment-options"
+import share from "./components/moment-share"
+import shares from "./components/moment-full-shares"
 import top_left_root from "./components/roots/top/moment-top_left_root"
 import top_right_root from "./components/roots/top/moment-top_right_root"
-import center_root from "./components/roots/center/moment-center_root"
-import bottom_root from "./components/roots/bottom/moment-bottom_root"
-import main_root from "./components/roots/moment-main_root"
-import container from "./components/moment-container"
-import like from "./components/moment-like"
-import description from "./components/moment-description"
-import date from "./components/moment-date"
-import share from "./components/moment-share"
-import more from "./components/moment-more"
+import top_root from "./components/roots/top/moment-top_root"
 import views from "./components/moment-full-views"
-import options from "./components/moment-options"
-import comments from "./components/moment-full-comments"
-import shares from "./components/moment-full-shares"
 
 export const Moment = {
     Root: {
@@ -24,6 +25,7 @@ export const Moment = {
         Center: center_root,
         Bottom: bottom_root,
     },
+    AudioControl: audio_control,
     ShareButton: share,
     MoreButton: more,
     LikeButton: like,
