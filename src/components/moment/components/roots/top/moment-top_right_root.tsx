b113fb8fd01468e309781c8ddd657a10ba0c7949
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-05-30 02:03:18 -0300

    refactor: adicionar estilo de margem ao componente MomentTopRightRoot para melhorar o layout e a consistência visual

--- src/components/moment/components/roots/top/moment-top_right_root.tsx
+++ src/components/moment/components/roots/top/moment-top_right_root.tsx
@@ -1,12 +1,15 @@
-import React from "react"
-import { View } from "react-native"
+import { View, ViewStyle } from "react-native"
+
 import { MomentTopRightRootProps } from "../../../moment-types"
+import React from "react"
+import sizes from "@/layout/constants/sizes"
 
 export default function top_right_root({ children }: MomentTopRightRootProps) {
-    const container: any = {
+    const container: ViewStyle = {
         flexDirection: "row",
         alignItems: "center",
         justifyContent: "flex-end",
+        marginRight: sizes.margins["2sm"],
     }
 
     return <View style={container}>{children}</View>
