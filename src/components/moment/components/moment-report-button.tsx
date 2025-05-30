9f0d6d883438703a31423a003e041dc3c0929936
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-05-30 02:07:42 -0300

    feat: adicionar botão de relatório ao componente MomentReportButton para permitir que usuários relatem momentos de forma interativa

---
+++ src/components/moment/components/moment-report-button.tsx
@@ -0,0 +1,30 @@
+import BottomSheetContext from "../../../contexts/bottomSheet"
+import ButtonStandart from "../../buttons/button-standart"
+import ColorTheme from "../../../layout/constants/colors"
+import MomentContext from "../context"
+import { MomentDateProps } from "../moment-types"
+import MoreIcon from "../../../assets/icons/svgs/ellipsis.svg"
+import React from "react"
+import { RenderReportModal } from "@/features/list-moments/components/feed/render-report-modal-feed"
+
+export default function ReportButton({
+    color = String(ColorTheme().text),
+}: MomentDateProps) {
+    const { expand } = React.useContext(BottomSheetContext)
+    const { momentOptions, momentData, momentUserActions} = React.useContext(MomentContext)
+    function handlePress() {
+        expand({
+            children: (
+                <RenderReportModal moment={{...momentData, is_liked: momentUserActions.liked}} />
+            ),
+            snapPoints: ["99%"],
+        })
+    }
+    if (momentOptions.enableReport) {
+        return (
+            <ButtonStandart action={handlePress} margins={false}style={{padding: 0, paddingHorizontal: 8, paddingVertical: 0, marginHorizontal: 0, marginVertical: 0, marginVertical: 0, backgroundColor: "transparent"}}>
+                <MoreIcon fill={color} width={20} height={20} />
+            </ButtonStandart>
+        )
+    } else return null
+}
