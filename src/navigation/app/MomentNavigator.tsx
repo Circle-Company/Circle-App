503c7eb859e10aa547a31d593592caf589e02227
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-06-02 11:13:16 -0300

    feat: adicionar telas NewMomentVideoScreen e VideoTrimmerScreen ao MomentNavigator, integrando funcionalidades de seleção e recorte de vídeos

--- src/navigation/app/MomentNavigator.tsx
+++ src/navigation/app/MomentNavigator.tsx
@@ -1,19 +1,22 @@
-import { createStackNavigator } from "@react-navigation/stack"
-import React from "react"
+import ColorTheme, { colors } from "../../layout/constants/colors"
+
+import { Interpolation as Horizontal } from "../transitions/horizontal-right"
+import LanguageContext from "../../contexts/Preferences/language"
 import MemoryHeaderLeft from "../../components/headers/memory/memory-header_left"
 import MomentFullHeaderLeft from "../../components/headers/moment/moment_full-header_left"
-import NewMomentImageRight from "../../components/headers/moment/new_moment_image-header_right"
-import NewMomentInputDescriptionRight from "../../components/headers/moment/new_moment_input_description-header_right"
-import NewMomentSelectMemoryRight from "../../components/headers/moment/new_moment_select_memory-header_right"
-import LanguageContext from "../../contexts/Preferences/language"
-import ColorTheme, { colors } from "../../layout/constants/colors"
-import Sizes from "../../layout/constants/sizes"
 import MomentFullScreen from "../../pages/app/Moment/moment-full"
 import NewMomentDescription from "../../pages/app/Moment/new_moment-description"
 import NewMomentGalleryScreen from "../../pages/app/Moment/new_moment-gallery"
+import NewMomentImageRight from "../../components/headers/moment/new_moment_image-header_right"
 import NewMomentImageScreen from "../../pages/app/Moment/new_moment-image"
+import NewMomentInputDescriptionRight from "../../components/headers/moment/new_moment_input_description-header_right"
 import NewMomentSelectMemory from "../../pages/app/Moment/new_moment-select_memory"
-import { Interpolation as Horizontal } from "../transitions/horizontal-right"
+import NewMomentSelectMemoryRight from "../../components/headers/moment/new_moment_select_memory-header_right"
+import NewMomentVideoScreen from "../../pages/app/Moment/new_moment-video"
+import React from "react"
+import Sizes from "../../layout/constants/sizes"
+import VideoTrimmerScreen from "../../pages/app/Moment/video-trimmer"
+import { createStackNavigator } from "@react-navigation/stack"
 
 const MomentStack = createStackNavigator()
 
@@ -68,6 +71,33 @@ export function MomentNavigator() {
                     cardStyleInterpolator: Horizontal,
                     headerLeft: () => <MemoryHeaderLeft />,
                     headerRight: () => <NewMomentImageRight />,
+                }}
+            />
+            <MomentStack.Screen
+                name="NewMomentVideoScreen"
+                component={NewMomentVideoScreen}
+                options={{
+                    headerTitle: t("New Moment"),
+                    headerStyle: [HeaderStyle],
+                    headerTitleStyle: { color: String(ColorTheme().text) },
+                    cardStyle: { backgroundColor: String(ColorTheme().background) },
+                    cardOverlayEnabled: true,
+                    cardStyleInterpolator: Horizontal,
+                    headerLeft: () => <MemoryHeaderLeft />,
+                    headerRight: () => <NewMomentImageRight />,
+                }}
+            />
+            <MomentStack.Screen
+                name="VideoTrimmerScreen"
+                component={VideoTrimmerScreen}
+                options={{
+                    headerTitle: t("Cut Video"),
+                    headerStyle: [HeaderStyle],
+                    headerTitleStyle: { color: String(ColorTheme().text) },
+                    cardStyle: { backgroundColor: String(ColorTheme().background) },
+                    cardOverlayEnabled: true,
+                    cardStyleInterpolator: Horizontal,
+                    headerLeft: () => <MemoryHeaderLeft />,
                 }}
             />
             <MomentStack.Screen
