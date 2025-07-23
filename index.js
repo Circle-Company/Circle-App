5d7c5f6db2fd3c6df74676bb6cd60bdda914bebf
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-07-23 10:31:07 -0300

    refactor: clean up index.js by removing unused imports and log suppression

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-import React from "react"
-import { AppRegistry, LogBox } from "react-native"
-
-LogBox.ignoreLogs([
-    "[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
-    "[DEPRECATED] Default export is deprecated. Instead use `import { create } from 'zustand'`.",
-])
-LogBox.ignoreLogs(["new NativeEventEmitter()"])
-
+import 'react-native-gesture-handler';
+import { AppRegistry } from "react-native"
+import { name as appName } from  "./app.json"
 import App from "./App"
-import { name as appName } from "./app.json"
 
-const Aplication = () => {
-    return <App />
-}
-
-AppRegistry.registerComponent(appName, () => Aplication)
+AppRegistry.registerComponent(appName, () => App)
