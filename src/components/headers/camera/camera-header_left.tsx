95338591244ad4208384a92f0111d8c6f2150fc8
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-12-11 18:36:05 -0300

    add: camera header

---
+++ src/components/headers/camera/camera-header_left.tsx
@@ -0,0 +1,26 @@
+import Icon from "@/assets/icons/svgs/close_bold.svg"
+import { useNavigation } from "@react-navigation/native"
+import React from "react"
+import { View, ViewStyle } from "react-native"
+import ColorTheme from "../../../constants/colors"
+import HeaderButton from "../headerButton"
+
+export default function HeaderLeft() {
+    const navigation = useNavigation()
+
+    const container: ViewStyle = {
+        flexDirection: "row",
+    }
+
+    return (
+        <View style={container}>
+            <HeaderButton
+                action={() => navigation.goBack()}
+                square
+                color={String(ColorTheme().backgroundDisabled)}
+            >
+                <Icon fill={String(ColorTheme().text)} width={13} height={13} />
+            </HeaderButton>
+        </View>
+    )
+}
