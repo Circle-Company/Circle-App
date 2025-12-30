095464d77364cbf8d75d5eb2eb7aac725bb25ce4
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-12-30 03:27:50 -0300

    refactor(features): remove legacy feature modules and screens superseded by new structure

--- src/features/list-moments/components/feed/render-comment-modal-feed.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native"
-
-export default function CommentsModal() {
-    return (
-        <View>
-            <Text>CommentsModalFeed</Text>
-        </View>
-    )
-}
