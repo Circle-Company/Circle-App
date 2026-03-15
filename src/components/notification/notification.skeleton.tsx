7092bd1100421a0445696f1c77e33726f79a75bb
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2026-03-15 12:02:53 -0300

    Add notification skeleton component

---
+++ src/components/notification/notification.skeleton.tsx
@@ -0,0 +1,19 @@
+import { ViewStyle } from "react-native"
+import { Skeleton } from "../skeleton"
+import sizes from "@/constants/sizes"
+import { colors } from "@/constants/colors"
+
+export function NotificationSkeleton() {
+    const container: ViewStyle = {
+        width: "100%",
+        alignItems: "flex-start",
+        justifyContent: "center",
+        minHeight: sizes.screens.height * 0.1,
+        paddingLeft: sizes.paddings["1sm"],
+        paddingRight: sizes.paddings["1md"],
+        paddingVertical: sizes.paddings["2sm"],
+        borderRadius: sizes.borderRadius["1md"] * 1.5,
+    }
+
+    return <Skeleton.View style={container}></Skeleton.View>
+}
